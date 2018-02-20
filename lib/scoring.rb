e2c52df2a888fb2fb35b6c0739485e375156d437
Cara <cararbrennan@gmail.com>
2018-02-20 14:42:59 -0800

    Passes first test

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,6 +1,25 @@
 module Scrabble
   class Scoring
     def self.score(word)
+
+
+      word_score = 0
+
+
+        if word.include? 'd'
+          word_score += 2
+        end
+
+        if word.include? 'o'
+          word_score += 1
+        end
+
+        if word.include? 'g'
+          word_score += 2
+        end
+
+
+      return word_score
     end
 
     def self.highest_score_from(array_of_words)
