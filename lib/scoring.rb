647c7698711c7b68c0f9c804bc504df1f1dda73a
Steffnay <steffannbrown@gmail.com>
2018-02-20 15:30:18 -0800

    passes all 4 tests

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -5,23 +5,32 @@ module Scrabble
       @letters_array = @word.split(//)
       total = 0
 
+      # returns nil if word > 7 letters
+      if @letters_array.length > 7
+        return nil
+      end
+      
       # original word score
       @letters_array.each do |letter|
-        case
-        when letter == "d" || letter == "g"
-          total += 2
-        when letter == "b" || letter == "c" || letter == "m" || letter == "p"
-          total += 3
-        when letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y"
-          total += 4
-        when letter == "k"
-          total += 5
-        when letter == "j" || letter == "x"
-          total += 8
-        when letter == "q" || letter == "z"
-          total += 10
+        if ("a".."z").cover?(letter)
+          case
+          when letter == "d" || letter == "g"
+            total += 2
+          when letter == "b" || letter == "c" || letter == "m" || letter == "p"
+            total += 3
+          when letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y"
+            total += 4
+          when letter == "k"
+            total += 5
+          when letter == "j" || letter == "x"
+            total += 8
+          when letter == "q" || letter == "z"
+            total += 10
+          else
+            total += 1
+          end
         else
-          total += 1
+          return nil
         end
       end
 
