18b08cf8afd61eacd26673a177d938e20bc7feac
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-02-20 14:36:53 -0800

    create part 2 score card

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,6 +1,11 @@
+
 module Scrabble
   class Scoring
+
+    score_chart = {"F" => 4, "H" => 4, "V" => 4, "W" =>4, "Y" => 4, "K" => 5, "J"=>8, "X" =>8, "Q" =>10, "Z" => 10}
+    
     def self.score(word)
+
     end
 
     def self.highest_score_from(array_of_words)
