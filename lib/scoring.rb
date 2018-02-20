4fe7c5972e05f6788d0aa98f2cc32de3167c6bea
emilcecarlisa <emilcearlisa@gmail.com>
2018-02-20 15:27:47 -0800

    Sorry, small & irrelevant changes

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -4,28 +4,30 @@ module Scrabble
 
 
       word_score = 0
+      word_array = word.split
 
-        word.each do |letter|
-          if 'aeioulnrst'.include? |letter|
-            word_score += 1
-          elsif 'dg'.include? |letter|
-            word_score += 2
-          elsif 'bcmp'.include? |letter|
-            word_score += 3
-          elsif 'fhvwy'.include? |letter|
-            word_score += 4
-          elsif 'k'.include? |letter|
-            word_score += 5
-          elsif 'jx'.include? |letter|
-            word_score += 8
-          elsif 'qz'.include? |letter|
-            word_score += 10
-          end
-
-          return word_score
+      word_array.each do |letter|
+        if 'aeioulnrst'.include? letter
+          word_score += 1
+        elsif 'dg'.include? letter
+          word_score += 2
+        elsif 'bcmp'.include? letter
+          word_score += 3
+        elsif 'fhvwy'.include? letter
+          word_score += 4
+        elsif 'k'.include? letter
+          word_score += 5
+        elsif 'jx'.include? letter
+          word_score += 8
+        elsif 'qz'.include? letter
+          word_score += 10
         end
 
-        def self.highest_score_from(array_of_words)
-        end
+        return word_score
+      end
+
+      def self.highest_score_from(array_of_words)
       end
     end
+  end
+end
