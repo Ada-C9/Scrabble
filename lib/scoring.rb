94a6e53f3aad258f924b879055153e8bc18984f2
Tor Shimizu <torshimizu@gmail.com>
2018-02-20 14:21:25 -0800

    created data structure for letter values

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,6 +1,17 @@
 module Scrabble
   class Scoring
     def self.score(word)
+      letter_values = {
+        ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'] => 1,
+        ['d', 'g'] => 2,
+        ['b', 'c', 'm', 'p'] => 3,
+        ['f', 'h', 'v', 'w', 'y'] => 4,
+        ['k'] => 5,
+        ['j', 'x'] => 8,
+        ['q', 'z'] => 10
+      }
+
+      
     end
 
     def self.highest_score_from(array_of_words)
