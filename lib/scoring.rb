fd49719be1a02aa558ae104bd65f421b02b87735
Angela Poland <angelalpoland@gmail.com>
2018-02-20 14:14:11 -0800

    added score chart hash

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,43 @@
 module Scrabble
   class Scoring
+
+    score_chart = {
+      "A" => 1,
+      "B" => 3,
+      "C" => 3,
+      "D" => 2,
+      "E" => 1,
+      "F" => 4,
+      "G" => 2,
+      "H" => 4,
+      "I" => 1,
+      "J" => 8,
+      "K" => 5,
+      "L" => 1,
+      "M" => 3,
+      "N" => 1,
+      "O" => 1,
+      "P" => 3,
+      "Q" => 10,
+      "R" => 1,
+      "S" => 1,
+      "T" => 1,
+      "U" => 1,
+      "V" => 4,
+      "W" => 4,
+      "X" => 8,
+      "Y" => 4,
+      "Z" => 10
+    }
+
+
+
+
+
     def self.score(word)
     end
 
     def self.highest_score_from(array_of_words)
     end
-  end
-end
+  end #end of class
+end #end of module
