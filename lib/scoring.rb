224cb5df0eae35a3a83e8b376927b297a6fa19c8
emilcecarlisa <emilcearlisa@gmail.com>
2018-02-20 15:13:17 -0800

    Added all letters and corresponding scores

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -5,24 +5,27 @@ module Scrabble
 
       word_score = 0
 
+        word.each do |letter|
+          if 'aeioulnrst'.include? |letter|
+            word_score += 1
+          elsif 'dg'.include? |letter|
+            word_score += 2
+          elsif 'bcmp'.include? |letter|
+            word_score += 3
+          elsif 'fhvwy'.include? |letter|
+            word_score += 4
+          elsif 'k'.include? |letter|
+            word_score += 5
+          elsif 'jx'.include? |letter|
+            word_score += 8
+          elsif 'qz'.include? |letter|
+            word_score += 10
+          end
 
-        if word.include? 'd'
-          word_score += 2
+          return word_score
         end
 
-        if word.include? 'o'
-          word_score += 1
+        def self.highest_score_from(array_of_words)
         end
-
-        if word.include? 'g'
-          word_score += 2
-        end
-
-
-      return word_score
+      end
     end
-
-    def self.highest_score_from(array_of_words)
-    end
-  end
-end
