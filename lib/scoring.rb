448bdb9024538bac34c45cd38a12b2b2d7eb56c1
Nora Peters <norapeters5@gmail.com>
2018-02-20 14:45:13 -0800

    first commit

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,52 @@
 module Scrabble
+
   class Scoring
+
+    LETTERVALUES = {
+      :A => 1,
+      :E => 1,
+      :I => 1,
+      :O => 1,
+      :U => 1,
+      :L => 1,
+      :N => 1,
+      :R => 1,
+      :S => 1,
+      :T => 1,
+      :D => 2,
+      :G => 2,
+      :B => 3,
+      :C => 3,
+      :M => 3,
+      :P => 3,
+      :F => 4,
+      :H => 4,
+      :V => 4,
+      :W => 4,
+      :Y => 4,
+      :K => 5,
+      :J => 8,
+      :X => 8,
+      :Q => 10,
+      :Z => 10
+    }
+    #     class Post
+    #   STATUS = Hash.new{ |hash, key| raise( "Key #{ key } is unknown" )}.update(
+    #     :draft => "draft",
+    #     :awaiting_review => "awaiting review",
+    #    :posted => "posted" )
+    # end
+
     def self.score(word)
+      # take input & convert to upcase
+
+      # split input
+      # check each letter of input against constant Hash and return the value of that key
     end
 
     def self.highest_score_from(array_of_words)
     end
+
   end
+
 end
