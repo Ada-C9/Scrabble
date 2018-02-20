a6494f29f8bf3727f171f3d75e9d1710229f29c8
Dikla Aharoni Rikovitch <dikla.aharoni@gmail.com>
2018-02-20 15:59:18 -0800

    finished the self.score method but didn't test it yet

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,6 +1,30 @@
 module Scrabble
   class Scoring
     def self.score(word)
+      def self.score(word)
+      total_score = 0
+      scrabble_word = word.split(//)
+      scrabble_word.each do |letter|
+        case letter.downcase
+        when "a", "e", "i", "o", "u", "l", "n", "r", "s", "t"
+          total_score += 1
+        when "d", "g"
+          total_score += 2
+        when "b", "c", "m", "p"
+          total_score += 3
+        when "f", "h", "v", "w", "y"
+          total_score += 4
+        when "k"
+          total_score += 5
+        when "j", "x"
+          total_score += 8
+        when "q", "z"
+          total_score += 10
+        end
+      end
+      return total_score
+    end
+
     end
 
     def self.highest_score_from(array_of_words)
