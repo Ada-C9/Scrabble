9076a5f9ecaebe11875cc88d8b4efc120acb62ff
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-20 15:26:28 -0800

    Added functionality to self.score method, passed all self.score tests.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,46 @@
 module Scrabble
   class Scoring
+
     def self.score(word)
+      total = 0
+      array_of_characters = word.upcase.split('')
+      if word.match(/^[a-zA-Z]{1,7}$/) == nil
+        total = nil
+      else
+        array_of_characters.each do |letter|
+          scrabble_value = letter
+          case scrabble_value
+            when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
+              scrabble_value = 1
+            when "D", "G"
+              scrabble_value = 2
+            when "B", "C", "M", "P"
+              scrabble_value = 3
+            when "F", "H", "V", "W", "Y"
+              scrabble_value = 4
+            when "K"
+              scrabble_value = 5
+            when "J","X"
+              scrabble_value = 8
+            when "Q", "Z"
+              scrabble_value = 10
+            end
+            total = scrabble_value + total
+          end
+      end
+      if array_of_characters.length == 7
+        total = total + 50
+      end
+      return total
     end
 
     def self.highest_score_from(array_of_words)
+      if array_of_words.size == 0
+        highest_score = nil
+      elsif array_of_words.size == 1
+        highest_score == array_of_words[0]
+      end
+      return highest_score
     end
   end
 end
