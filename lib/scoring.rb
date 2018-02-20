07c17b3abcffa2eecd391bf1b793dae24ec4f8d7
Anne <annehwatson@gmail.com>
2018-02-20 14:26:10 -0800

    adding comments

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,18 @@
 module Scrabble
   class Scoring
-    def self.score(word)
+    def self.score(word) #word = "string of letters".downcase
+      # return a number based on the point chart
+      # 7 letter bonus (use .length)
+
     end
 
     def self.highest_score_from(array_of_words)
+      # use score method
+      # find the highest score
+      # if tie:
+      # fewest tiles used unless the other word used all 7 then that wins
+      # if score & length are same for both high words, the first one wins
+
     end
   end
 end
