aef9c1c298ff939378a9f83708140a8727094a28
Steffnay <steffannbrown@gmail.com>
2018-02-20 15:12:00 -0800

    passing first 3 tests

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,16 +1,18 @@
 module Scrabble
   class Scoring
     def self.score(word)
-      @word = word
+      @word = word.downcase
       @letters_array = @word.split(//)
       total = 0
+
+      # original word score
       @letters_array.each do |letter|
         case
         when letter == "d" || letter == "g"
           total += 2
         when letter == "b" || letter == "c" || letter == "m" || letter == "p"
           total += 3
-        when letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "Y"
+        when letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y"
           total += 4
         when letter == "k"
           total += 5
@@ -22,8 +24,13 @@ module Scrabble
           total += 1
         end
       end
-        puts total
-        return total
+
+      # word-length scoring bonus
+      if @letters_array.length == 7
+        total += 50
+      end
+
+      return total
     end
 
     def self.highest_score_from(array_of_words)
