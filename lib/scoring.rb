c0b7f76b00d2dd3b11368041f06392493db62e3a
Madaleine Shields <maddiev.shields@gmail.com>
2018-02-20 15:57:47 -0800

    Notes on how to find score

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,8 +1,8 @@
 module Scrabble
   class Scoring
 
-    def initialize
-      @scores = {
+    def self.score(word)
+      @all_letters = {
         "A"=>1, "B"=>3, "C"=>3, "D"=>2,
         "E"=>1, "F"=>4, "G"=>2, "H"=>4,
         "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -11,9 +11,14 @@ module Scrabble
         "U"=>1, "V"=>4, "W"=>4, "X"=>8,
         "Y"=>4, "Z"=>10
       }
-    end
 
-    def self.score(word)
+      @all_letters.each {|letter, points_per_letter| print letter, " ", points_per_letter, "\n"}
+
+      # split word >> goes into array
+      # check if
+      # if letter array is a key, return the value of that
+      # add the values
+
       return 5
     end
   end
