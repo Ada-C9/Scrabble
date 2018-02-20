27059d0ddc10ee1d17f678aff8c2e8e57432e263
Leticia Tran <leticiastran@gmail.com>
2018-02-20 14:27:37 -0800

    Added scoring method and fixed typo on the 'game' files - First score test OK

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,39 @@
+
 module Scrabble
   class Scoring
+
     def self.score(word)
+
+      # TODO: fix it, better way to evaluate, with arrays.
+      letters_array = word.upcase.split("")
+
+      points = 0
+      letters_array.each do |letter|
+
+        if letter == "Q" || letter == "Z"
+          points += 10
+        elsif letter == "D" || letter == "G"
+          points += 2
+        elsif letter =="B" || letter == "C" || letter =="M" || letter == "P"
+          points += 3
+        elsif letter == "F" || letter == "H" || letter == "V" || letter == "W" || letter == "Y"
+          points += 4
+        elsif letter == "K"
+          points += 5
+        elsif letter == "J" || letter == "X"
+          points += 8
+        else #"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
+          points += 1
+        end
+      end
+        return points
     end
+
 
     def self.highest_score_from(array_of_words)
     end
-  end
-end
+  end #Scoring
+end #Scrabble
+
+
+Scrabble::Scoring.score("dog")
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -15,32 +15,32 @@ describe 'Scoring' do
       Scrabble::Scoring.score('pig').must_equal 6
     end
 
-    it 'adds 50 points for a 7-letter word' do
+    xit 'adds 50 points for a 7-letter word' do
       Scrabble::Scoring.score('academy').must_equal 65
     end
 
-    it 'handles all upper- and lower-case letters' do
+    xit 'handles all upper- and lower-case letters' do
       Scrabble::Scoring.score('dog').must_equal 5
       Scrabble::Scoring.score('DOG').must_equal 5
       Scrabble::Scoring.score('DoG').must_equal 5
     end
 
-    it 'returns nil for strings containing bad characters' do
+    xit 'returns nil for strings containing bad characters' do
       Scrabble::Scoring.score('#$%^').must_be_nil
       Scrabble::Scoring.score('char^').must_be_nil
       Scrabble::Scoring.score(' ').must_be_nil
     end
 
-    it 'returns nil for words > 7 letters' do
+    xit 'returns nil for words > 7 letters' do
       Scrabble::Scoring.score('abcdefgh').must_be_nil
     end
 
-    it 'returns nil for empty words' do
+    xit 'returns nil for empty words' do
       Scrabble::Scoring.score('').must_be_nil
     end
   end
 
-  describe 'highest_score_from' do
+  xdescribe 'highest_score_from' do
     it 'returns nil if no words were passed' do
     end
 
