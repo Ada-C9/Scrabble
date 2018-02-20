d309af54a37bb3592fcc6d752661f91aa5913f43
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-02-20 15:58:58 -0800

    First commit: I updated the Scoring#self.score method that will now take in the word argument and split the word into an array. Then the case statement will go through the word array and return the total score.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,58 @@
+require 'awesome_print'
+require 'pry'
+
 module Scrabble
+
   class Scoring
+
     def self.score(word)
+      total_score = 0
+      scrabble_word = word.split(//)
+      scrabble_word.each do |letter|
+        case letter.downcase
+        when "a", "e", "i", "o", "u", "l", "n", "r", "s", "t"
+          total_score += 1
+        when "d", "g"
+          total_score += 2
+        when "b", "c", "m", "p"
+          total_score += 3
+        when "f", "h", "v", "w", "y"
+          total_score += 4
+        when "k"
+          total_score += 5
+        when "j", "x"
+          total_score += 8
+        when "q", "z"
+          total_score += 10
+        end
+      end
+      return total_score
     end
+
 
     def self.highest_score_from(array_of_words)
     end
-  end
-end
+
+  end # class Scoring
+
+end # module Scrabble
+
+
+
+test_chart = Scrabble::Scoring.score('cat')
+ap test_chart
+
+# binding.pry
+
+
+# # array reference
+# scrabble_chart = [
+#   { 1 => %w[A E I O U L N R S T a e i o u l n r s t] },
+#   { 2 => %w[D G d g]},
+#   { 3 => %w[B C M P b c m p]},
+#   { 4 => %w[F H V W Y f h v w y]},
+#   { 5 => %w[K k]},
+#   { 8 => %w[J X j x]},
+#   { 10 => %w[Q Z q z]}
+# ]
+# ap scrabble_chart[0].values[0][0]
