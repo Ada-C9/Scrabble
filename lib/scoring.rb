06d58d74e51ef41635faff743ba05d496443530d
Alexandria Brown <axbn2190@gmail.com>
2018-02-20 14:42:17 -0800

    built letters_hash, testing score

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,37 @@
+require 'awesome_print'
+require 'pry'
+
 module Scrabble
   class Scoring
-    def self.score(word)
+    attr_reader :word, :score
+
+    @@letters_hash = { ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"] => 1, ["d", "g"] => 2, ["b", "c", "m", "p"] => 3, ["f", "h", "v", "w", "y"] => 4, ["k"] => 5, ["j", "x"] => 8, ["q", "z"] => 10 }
+
+
+
+    def initialize
+      @word = word
     end
 
+    def self.score(word)
+
+      score = []
+
+      dog_array = word.split(//)
+
+      dog_array.each do |i|
+        @@letters_hash.include? i
+        letter = @@letters_hash[i]
+        score << letter
+      end
+      return score.sum
+    end
+
+
     def self.highest_score_from(array_of_words)
+
     end
   end
 end
+
+# ap Scrabble::Scoring.score('dog')
