1c34c1299983aa1b3b8c123bd7eca4f29a3a7e31
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-20 15:00:47 -0800

    Starting code for scoring.rb

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,9 +1,28 @@
 module Scrabble
   class Scoring
-    def self.score(word)
+
+    def initialize
+      @scores = {
+        1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+        2 => ["D", "G"],
+        3 => ["B", "C", "M", "P"],
+        4 => ["F", "H", "V", "W", "Y"],
+        5 => ["K"],
+        8 => ["J", "X"],
+        10 => ["Q", "Z"]
+      }
     end
 
-    def self.highest_score_from(array_of_words)
+    def self.score(word)
+      count = 0
+        @scores.length.each do |key, value|
+          count += @scores.key
+        end
     end
   end
 end
+
+#     def self.highest_score_from(array_of_words)
+#     end
+#   end
+# end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -15,48 +15,48 @@ describe 'Scoring' do
       Scrabble::Scoring.score('pig').must_equal 6
     end
 
-    it 'adds 50 points for a 7-letter word' do
+    xit 'adds 50 points for a 7-letter word' do
       Scrabble::Scoring.score('academy').must_equal 65
     end
 
-    it 'handles all upper- and lower-case letters' do
+    xit 'handles all upper- and lower-case letters' do
       Scrabble::Scoring.score('dog').must_equal 5
       Scrabble::Scoring.score('DOG').must_equal 5
       Scrabble::Scoring.score('DoG').must_equal 5
     end
 
-    it 'returns nil for strings containing bad characters' do
+    xit 'returns nil for strings containing bad characters' do
       Scrabble::Scoring.score('#$%^').must_be_nil
       Scrabble::Scoring.score('char^').must_be_nil
       Scrabble::Scoring.score(' ').must_be_nil
     end
 
-    it 'returns nil for words > 7 letters' do
+    xit 'returns nil for words > 7 letters' do
       Scrabble::Scoring.score('abcdefgh').must_be_nil
     end
 
-    it 'returns nil for empty words' do
+    xit 'returns nil for empty words' do
       Scrabble::Scoring.score('').must_be_nil
     end
   end
 
   describe 'highest_score_from' do
-    it 'returns nil if no words were passed' do
+    xit 'returns nil if no words were passed' do
     end
 
-    it 'returns the only word in a length-1 array' do
+    xit 'returns the only word in a length-1 array' do
     end
 
     it 'returns the highest word if there are two words' do
     end
 
-    it 'if tied, prefer a word with 7 letters' do
+    xit 'if tied, prefer a word with 7 letters' do
     end
 
-    it 'if tied and no word has 7 letters, prefers the word with fewer letters' do
+    xit 'if tied and no word has 7 letters, prefers the word with fewer letters' do
     end
 
-    it 'returns the first word of a tie with same letter count' do
+    xit 'returns the first word of a tie with same letter count' do
     end
   end
 end
