f0d882443c656f3d3957fe36cedef758dfba60a3
Mary Lamkin <mmlamkin@gmail.com>
2018-02-21 15:23:19 -0800

    Defines plays and play methods for class player.

--- lib/player.rb
+++ lib/player.rb
@@ -4,10 +4,16 @@ require_relative 'scoring'
 
 module Scrabble
   class Player
-    attr_reader :name
+    attr_reader :name, :plays
 
     def initialize(name)
       @name = name
+      @plays = []
+    end
+
+    def play(word)
+      @plays << word
+      word =
     end
 
   end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -8,13 +8,41 @@ require_relative '../lib/scoring'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Player' do
+
+  before do
+    @john = Scrabble::Player.new("John")
+  end
+
   describe 'initialize' do
     it 'Takes in a name' do
-      john = Scrabble::Player.new("John")
+      @john.must_respond_to :name
+      @john.name.must_equal "John"
+      @john.name.must_be_kind_of String
+    end
 
-      john.must_respond_to :name
-      john.name.must_equal "John"
-      john.name.must_be_kind_of String
+    it 'returns an array' do
+      @john.plays.must_be_kind_of Array
+      @john.plays.must_equal []
+    end
+  end
+
+  describe 'play' do
+    it 'takes in a word' do
+      @john.must_respond_to :play
+    end
+
+    it 'word is added to play array' do
+      @john.play("snail")
+      @john.plays.must_include "snail"
+    end
+
+    xit 'returns false if game is over (player has already won)' do
+
+    end
+
+    it 'returns score of word' do
+      @john.play("snail").must_equal 4
+      @john.play("snail").must_be_kind_of Integer
     end
   end
 end
