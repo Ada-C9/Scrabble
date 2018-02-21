ceb7aa3f3f8b3d91548b8739a2159140473bd63f
Kat <kat.eastman@gmail.com>
2018-02-21 15:14:05 -0800

    Stubbed out Player

--- lib/player.rb
+++ lib/player.rb
@@ -11,11 +11,26 @@ module Scrabble
       if won?
         return false
       else
+        return Scrabble::Scoring.score(word)
       end
-
     end
 
+    def total_score
+      #  Returns the sum of scores of played words
+    end
 
-  end
+    def highest_scoring_word
+      # Returns the highest scoring played word
+    end
 
-end
+    def highest_word_score
+      # Returns the highest_scoring_word score
+    end
+
+    private
+    def won?
+      # If the player has over 100 points, returns true, otherwise returns false
+    end
+
+  end # Player class end
+end # Scrabble end
