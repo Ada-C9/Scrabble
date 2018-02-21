bc295deea43ddd3570ed642f31b8a20216d368d1
Maja Graonic <m.graonic@gmail.com>
2018-02-20 16:13:36 -0800

    stopped using regex and used conditionals instead

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -4,41 +4,47 @@ module Scrabble
   class Scoring
 
     @letters = {
-       "A"=>1,
-       "B"=>3,
-       "C"=>3,
-       "D"=>2,
-       "E"=>1,
-       "F"=>4,
-       "G"=>2,
-       "H"=>4,
-       "I"=>1,
-       "J"=>8,
-       "K"=>5,
-       "L"=>1,
-       "M"=>3,
-       "N"=>1,
-       "O"=>1,
-       "P"=>3,
-       "Q"=>10,
-       "R"=>1,
-       "S"=>1,
-       "T"=>1,
-       "U"=>1,
-       "V"=>4,
-       "W"=>4,
-       "X"=>8,
-       "Y"=>4,
-       "Z"=>10
-     }
+      "A"=>1,
+      "B"=>3,
+      "C"=>3,
+      "D"=>2,
+      "E"=>1,
+      "F"=>4,
+      "G"=>2,
+      "H"=>4,
+      "I"=>1,
+      "J"=>8,
+      "K"=>5,
+      "L"=>1,
+      "M"=>3,
+      "N"=>1,
+      "O"=>1,
+      "P"=>3,
+      "Q"=>10,
+      "R"=>1,
+      "S"=>1,
+      "T"=>1,
+      "U"=>1,
+      "V"=>4,
+      "W"=>4,
+      "X"=>8,
+      "Y"=>4,
+      "Z"=>10
+    }
 
 
     def self.score(word)
-      word_split = word.upcase.split(//)
-      word_value = 0
-      word_split.each do |one_letter|
-        if @letters.has_key?(one_letter)
-          word_value += @letters[one_letter]
+      if word.class != String
+        return NIL
+      else
+        word_value = 0
+        word_split = word.upcase.split(//)
+        word_split.each do |one_letter|
+          if @letters.has_key?(one_letter)
+            word_value += @letters[one_letter]
+          else
+            return nil
+          end
         end
       end
       if word_split.length >= 7
@@ -47,11 +53,9 @@ module Scrabble
       return word_value
     end
 
+
     def self.highest_score_from(array_of_words)
     end
   end
-  # if !word.class? String || !word.match?(/^[A-z]*$/)
-  #   return nil
-  # else
   # end
 end
