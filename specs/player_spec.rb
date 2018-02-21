f390f5de1b3805a55c9ad5d7eb1777b85d097156
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-02-21 15:02:09 -0800

    Wrote test for Player initialize method

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -5,4 +5,19 @@ require 'minitest/skip_dsl'
 require_relative '../lib/player'
 
 describe "Scrabble Player" do
+  let(:player) { Scrabble::Player.new("Anonymous")}
+  describe "#initialize" do
+    it "validates and instantiates the name instance variable" do
+      player.must_respond_to :name
+      player.name.must_be_instance_of String
+      player.name.must_equal "Anonymous"
+    end
+    it "#plays returns an Array of the words played by the player
+    " do
+    player.plays.must_respond_to :plays
+    player.plays.must_be_instance_of Array
+    player.plays.must_equal []
+  end
+
+end
 end
