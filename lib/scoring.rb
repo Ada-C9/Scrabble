12ed2fb1fa0a1995213df6ca4eee46691ec6e696
Samantha Berk <sberk10@gmail.com>
2018-02-21 15:24:43 -0800

    Stored the letter values in a constant and cleaned up the self.score method to calculate total score, and cleaned up the ternary statement to add 50 points to a 7 letter word

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -2,35 +2,48 @@ require 'pry'
 
 module Scrabble
   class Scoring
+    LETTERS = {
+      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+      2 => ["D", "G"],
+      3 => ["B", "C", "M", "P"],
+      4 => ["F", "H", "V", "W", "Y"],
+      5 => ["K"],
+      8 => ["J", "X"],
+      10 => ["Q", "Z"]
+    }
     def self.score(word) # 'dog'
       word = word.upcase
-      word_array = word.split('') #['d', 'o', 'g']
+      word_array = word.split('') #['D', 'O', 'G']
       total_score = 0
 
       if word_array.length > 7 || word_array.length < 1
         return total_score = nil
       else
         # add 50 pts if the word is 7 letters long
-        word_array.length == 7 ? total_score += 50 : total_score = 0
+        total_score += 50 if word_array.length == 7
         word_array.each do |letter|
-          case letter
-          when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
-            total_score += 1
-          when "D", "G"
-            total_score += 2
-          when "B", "C", "M", "P"
-            total_score += 3
-          when "F", "H", "V", "W", "Y"
-            total_score += 4
-          when "K"
-            total_score += 5
-          when "J", "X"
-            total_score += 8
-          when "Q", "Z"
-            total_score += 10
-          else
-            total_score = nil
-          end # case letter
+          score = LETTERS.find { |key, values|
+            values.include?(letter)
+          }.first
+          total_score += score
+          # case letter
+          # when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
+          #   total_score += 1
+          # when "D", "G"
+          #   total_score += 2
+          # when "B", "C", "M", "P"
+          #   total_score += 3
+          # when "F", "H", "V", "W", "Y"
+          #   total_score += 4
+          # when "K"
+          #   total_score += 5
+          # when "J", "X"
+          #   total_score += 8
+          # when "Q", "Z"
+          #   total_score += 10
+          # else
+          #   total_score = nil
+          # end # case letter
         end # word_array.each
         return total_score
       end # if > 7 statement
@@ -45,12 +58,14 @@ module Scrabble
         score_tracker[word] = word_score
       end # array_of_words.each
       max = Hash[score_tracker.select { |k, v| v == score_tracker.values.max}]
-        max.each do |k,v|
-          unless k.length == 7
+      max.each do |k,v|
+        unless k.length == 7
           return k.length.min
         end # max.each
       end # score_tracker
-     # score_tracker.each {|k,v|return k if v == score_tracker.values.max}
+      # score_tracker.each {|k,v|return k if v == score_tracker.values.max}
     end # self.highest_score_from
   end # Scoring class
 end # Scrabble module
+
+puts Scrabble::Scoring.score('dogrrrr')
