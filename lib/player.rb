52934e0799cceacef078db3c99f81c253d45c0bb
Aruna <aruna.thungathurthi@gmail.com>
2018-02-21 15:39:16 -0800

    sets up enivironment for player

---
+++ lib/player.rb
@@ -0,0 +1,10 @@
+require 'awesome_print'
+
+module Scrabble
+  class Player
+    def initialize(name)
+      
+
+    end
+  end
+end
---
+++ specs/player_spec.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../lib/player'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
