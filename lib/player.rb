b8fcb81d26896429530fcef688b4e9e33f885cde
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-02-22 14:53:25 -0800

    Wrote functionality for some Player tests and passed tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,38 @@
+require_relative 'scoring'
+
 module Scrabble
 
   class Player
 
-    attr_reader :name
-    
+    attr_reader :name, :plays
+
     def initialize(name)
       @name = name
+      @plays = []
+    end
+
+    def play(word)
+      if self.won?
+        return false
+      else
+        @plays << word
+        return Scrabble::Scoring.score(word)
+      end
+    end
+
+    def total_score
+      total_score = @plays.map do |word|
+        Scrabble::Scoring.score(word)
+      end.sum
+      return total_score
+    end
+
+    def won?
+      if self.total_score > 100
+        return true
+      else
+        return false
+      end
     end
 
 
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -21,28 +21,31 @@ describe 'Player' do
   end # describe name
 
   describe '#plays' do
+    it "returns an an empty Array if no words have been played" do
+      player_six = Scrabble::Player.new("player_six")
+      result = player_six.plays
+      result.must_be_kind_of Array
+      result.must_equal []
+    end
 
     it "returns an Array of the words played by the player" do
-      player.play("first")
-      player.play("second")
-      result = player.plays
+      player_eight = Scrabble::Player.new("player_eight")
+      player_eight.play("first")
+      player_eight.play("second")
+      result = player_eight.plays
       result.must_be_kind_of Array
       result.must_equal ["first", "second"]
     end
 
-    it "returns an an empty Array if no words have been played" do
-      result = player.plays
-      result.must_be_kind_of Array
-      result.must_equal []
-    end
 
   end # describe plays
 
   describe '#play(word)' do
 
     it "adds the input word to the plays Array" do
-      player.play("academy")
-      result = player.plays
+      player_two = Scrabble::Player.new("player_two")
+      player_two.play("academy")
+      result = player_two.plays
       result.must_equal ["academy"]
     end
 
@@ -55,7 +58,8 @@ describe 'Player' do
     end
 
     it "returns the score of the word" do
-      result = player.play("qqqq")
+      player_five = Scrabble::Player.new("player_five")
+      result = player_five.play("qqqq")
       result.must_equal 40
     end
 
@@ -64,27 +68,34 @@ describe 'Player' do
   describe '#total_score' do
 
     it "returns the sum of scores of played words" do
-      player.play("qqqq") # 40
-      player.play("zzzz") # 40
-      result = player.total_score
+      player_three = Scrabble::Player.new("player_three")
+      player_three.play("qqqq") # 40
+      player_three.play("zzzz") # 40
+      result = player_three.total_score
       result.must_equal 80
     end
 
     it "returns 0 if no words have been played" do
-      result = player.total_score
+      player_four = Scrabble::Player.new("player_four")
+      result = player_four.total_score
       result.must_equal 0
     end
 
   end # describe total_score
 
-  xdescribe '#won?' do
+  describe '#won?' do
 
-    it "returns true if the player has over 100 points" do
+
+    player_seven = Scrabble::Player.new("player_seven")
+    it "returns false if the player does not have over 100 points" do
+      player_seven.play("qqqqq")
+      player_seven.play("zzzzz")
+      player_seven.won?.must_equal false
 
     end
-
-    it "returns false if the player does not have over 100 points" do
-
+    it "returns true if the player has over 100 points" do
+      player_seven.play("a")
+      player_seven.won?.must_equal true
     end
 
   end # describe won?
