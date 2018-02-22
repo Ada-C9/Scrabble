ea8908d2db7f2e39053325f56eddb3ad91ac704d
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-21 19:31:20 -0800

    refactored data structure for scoring values

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,41 +1,29 @@
 module Scrabble
+  MAX_LETTERS = 7
   class Scoring
+
+    POINT_VALUES = {
+      "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2,
+      "H" => 4, "I" => 1, "J" => 8, "K" => 5, "L" => 1, "M" => 3, "N" => 1,
+      "O" => 1, "P" => 3, "Q" => 10, "R" => 1, "S" => 1, "T" => 1, "U" => 1,
+      "V" => 4, "W" => 4, "X" => 8, "Y" => 4,"Z" => 10
+    }
+
     def self.score(word)
       return if !has_valid_initial_word?(word)
       score = 0
       letters = word.split("")
       letters.each do |letter|
-        letter_score = score_value(letter.upcase)
+        letter_score = POINT_VALUES[letter.upcase]
         return if letter_score.nil?
         score += letter_score
       end
-      score += 50 if letters.size == 7
+      score += 50 if letters.size == MAX_LETTERS
       return score
     end
 
     def self.has_valid_initial_word?(word)
-      return word.class == String && word.size.between?(1, 7)
-    end
-
-    def self.score_value(letter)
-      one_point = %w[A E I O U L N R S T]
-      three_points = %w[B C M P]
-      four_points = %w[F H V W Y]
-      if one_point.include?(letter)
-        1
-      elsif letter == "D" || letter == "G"
-        2
-      elsif three_points.include?(letter)
-        3
-      elsif four_points.include?(letter)
-        4
-      elsif letter == "K"
-        5
-      elsif letter == "J" || letter == "X"
-        8
-      elsif letter == "Q" || letter == "Z"
-        10
-      end
+      return word.class == String && word.size.between?(1, MAX_LETTERS)
     end
 
     def self.highest_score_from(array_of_words)
@@ -62,10 +50,10 @@ module Scrabble
     end
 
     def self.break_tie(winning_words)
-      word_size = 7
+      word_size = MAX_LETTERS
       winning_word = ""
       winning_words.each do |word|
-        return word if word.size == 7
+        return word if word.size == MAX_LETTERS
         next if word.length > word_size
         if word.length < word_size
           word_size = word.length
