6f7e0e1d327d9fc8b3e2655bc42db3dc5902a99c
Anne <annehwatson@gmail.com>
2018-02-22 15:46:59 -0800

    player_spec version 1

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -8,67 +8,70 @@ require_relative '../lib/player'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Player' do
-  describe 'score' do
-    it 'correctly scores simple words' do
-      Scrabble::Scoring.score('dog').must_equal 5
-      Scrabble::Scoring.score('cat').must_equal 5
-      Scrabble::Scoring.score('pig').must_equal 6
-    end
-
-    it 'adds 50 points for a 7-letter word' do
-      Scrabble::Scoring.score('academy').must_equal 65
-    end
-
-    it 'handles all upper- and lower-case letters' do
-      Scrabble::Scoring.score('dog').must_equal 5
-      Scrabble::Scoring.score('DOG').must_equal 5
-      Scrabble::Scoring.score('DoG').must_equal 5
-    end
-
-    it 'returns nil for strings containing bad characters' do
-      Scrabble::Scoring.score('#$%^').must_be_nil
-      Scrabble::Scoring.score('char^').must_be_nil
-      Scrabble::Scoring.score(' ').must_be_nil
-    end
-
-    it 'returns nil for words > 7 letters' do
-      Scrabble::Scoring.score('abcdefgh').must_be_nil
-    end
-
-    it 'returns nil for empty words' do
-      Scrabble::Scoring.score('').must_be_nil
+  describe 'name' do
+    it 'returns the value of the @name instance variable' do
+      Scrabble::Player.name("Ada").must_equal "Ada"
     end
   end
 
-  describe 'highest_score_from' do
-    it 'returns nil if no words were passed' do
-      empty_array = []
-      Scrabble::Scoring.highest_score_from(empty_array).must_be_nil
-    end
-
-    it 'returns the only word in a length-1 array' do
-      one_word_array = ["apple"]
-      Scrabble::Scoring.highest_score_from(one_word_array).must_equal("apple")
-    end
-
-    it 'returns the highest word if there are two words' do
-      two_word_array = ["apple", "cat"] #apple = 9; cat = 5
-      Scrabble::Scoring.highest_score_from(two_word_array).must_equal("apple")
-    end
-
-    it 'if tied, prefer a word with 7 letters' do
-      tie_array = ["lalalal", "foul"] #both 7 value
-      Scrabble::Scoring.highest_score_from(tie_array).must_equal("lalalal")
-    end
-
-    it 'if tied and no word has 7 letters, prefers the word with fewer letters' do
-      same_value_diff_letters = ["eeeeee", "daaaa"]
-      Scrabble::Scoring.highest_score_from(same_value_diff_letters).must_equal("daaaa")
-    end
-
-    it 'returns the first word of a tie with same letter count' do
-      same_value_same_letters = ["iii", "ooo"]
-      Scrabble::Scoring.highest_score_from(same_value_same_letters).must_equal("iii")
+  describe 'plays' do
+    it 'returns an Array of the words played by the player' do
+      Scrabble::Player.play("apple")
+      Scrabble::Player.play("cat")
+      Scrabble::Player.plays.must_equal(["apple", "cat"])
     end
   end
-end
+
+  describe 'play(word)'
+    it 'adds the input word to the plays array' do
+      Scrabble::Player.play("apple")
+      Scrabble::Player.plays.must_include("apple")
+    end
+
+    it 'prevents a player who has won from playing' do
+      Scrabble::Player.won? = true
+      Scrabble::Player.play("apple").must_equal(false)
+    end
+
+    it "returns the score of the word that is played if the player hasn't won" do
+      Scrabble::Player.won? = false
+      Scrabble::Player.play("apple").must_equal(9)
+    end
+  end
+
+  describe 'total_score'
+    it 'returns the sum of scores of played words' do
+      Scrabble::Player.play("apple")
+      Scrabble::Player.play("cat")
+      Scrabble::Player.total_score.must_equal(14)
+    end
+  end
+
+  describe 'won?' do
+    it 'returns true if the player has over 100 points' do
+      Scrabble::Player.total_score = 101
+      Scrabble::Player.won?.must_equal(true)
+    end
+
+    it 'returns false if the player has less than 101 points' do
+      Scrabble::Player.total_score = 99
+      Scrabble::Player.won?.must_equal(false)
+    end
+  end
+
+  describe 'highest_scoring_word' do
+    it 'returns the highest scoring played word' do
+      Scrabble::Player.play("apple")
+      Scrabble::Player.play("cat")
+      Scrabble::Player.highest_scoring_word.must_equal("apple")
+    end
+  end
+
+  describe 'highest_word_score' do
+    it 'returns the highest_scoring_word score number' do
+      Scrabble::Player.play("apple")
+      Scrabble::Player.play("cat")
+      Scrabble::Player.highest_word_score.must_equal(9)
+    end #ends it block
+  end #ends describe highest word score
+end #ends class block
