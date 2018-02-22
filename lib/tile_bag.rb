3f082aa5600f5d826c3eed30b0839fb0dfdfb686
Zheng Cao <chori84@gmail.com>
2018-02-22 10:32:10 -0800

    Create TileBag class and test file

---
+++ lib/tile_bag.rb
@@ -0,0 +1,14 @@
+module Scrabble
+  class TileBag
+
+    def initialize
+    end
+
+    def draw_tiles(num)
+    end
+
+    def tiles_remaining
+    end
+
+  end
+end
---
+++ specs/tile_bag_spec.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../lib/tile_bag'
+require 'pry'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe 'TileBag' do
+
+  describe 'initialize' do
+
+  end
+
+  describe 'draw_tiles' do
+
+  end
+
+  describe 'tiles_remaining' do
+    
+  end
+
+end
