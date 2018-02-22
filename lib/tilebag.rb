7dd1e85120e5829634a06275808110d6288103df
Ari <dominiquearia0@gmail.com>
2018-02-22 15:25:41 -0800

    Completed initialize for Tilebag

---
+++ lib/tilebag.rb
@@ -0,0 +1,45 @@
+module Scrabble
+  class Tilebag
+    TILE_BAG_LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+      "T", "U", "V", "W", "X", "Y", "Z"]
+
+    TILE_BAG_COUNT = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 1, 2, 1]
+
+
+      attr_reader :bag, :tiles_remaining
+
+      def initialize
+        @bag = []
+        TILE_BAG_COUNT.each_with_index do |count, index|
+          count.times do
+            tile = TILE_BAG_LETTERS[index]
+            @bag << tile
+          end
+        end
+
+        @tiles_remaining = 96
+
+      end
+
+
+      def draw_tiles(num)
+
+        if @tiles_remaining >= num
+          @bag.rand(num)
+          @tiles_remaining -= num
+          return @tiles_remaining
+        end
+
+        return nil
+      end
+
+
+
+
+
+
+
+
+
+  end
+end
---
+++ specs/tilebag_spec.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../lib/tilebag'
+
+# Get that nice colorized output
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+
+describe 'Tilebag' do
+  describe 'initialize' do
+    it 'returns bag' do
+
+      our_bag = Scrabble::Tilebag.new
+      our_bag.bag.must_be_kind_of Array
+      our_bag.bag.must_equal ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "X", "X", "Y"]
+
+    end
+
+    it 'returns remaining tiles' do
+
+      our_bag = Scrabble::Tilebag.new
+      our_bag.tiles_remaining.must_equal 96
+
+    end
+  end
+
+  describe ''
+
+
+
+end
