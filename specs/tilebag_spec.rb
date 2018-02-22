2a17ef9296d206a077b5c15a6b54bf2592d0dcb6
Mariko <marikoanderson@gmail.com>
2018-02-22 13:47:47 -0800

    Added describe blocks for #tilesdrawn and #tilesremaining

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -7,16 +7,31 @@ require_relative '../lib/tilebag'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'TileBag' do
+
   let(:tilebag) {Scrabble::TileBag.new}
+
   describe '#initialize' do
     it 'Should set up the instance with a collection of all default tiles' do
       tilebag.must_be_instance_of Array
       tilebag.length.must_equal 98
     end
 
-    it 'confirms correct number of tiles in tilebag'
+    it 'confirms correct number of tiles in tilebag' do
       tilebag.tiles.select {|tile| tile == 'A'}.must_equal 9
       tilebag.tiles.select {|tile| tile == 'B'}.must_equal 2
+    end
+
+  end
+
+  describe '#draw_tiles(num)' do
+    it 'returns a collection of random tiles, removes the tiles from the default set' do
+
+    end
+  end
+
+  describe '#tiles_remaining' do
+    it "returns the number of tiles remaining in the bag" do
+      
     end
   end
 
