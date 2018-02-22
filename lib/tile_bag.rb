75643cb2ba9dfcf4f26cdb6df2c6c8460cd2fa33
Lily Sky <lilyxsky@gmail.com>
2018-02-21 16:58:15 -0800

    Tile Bag specs and working file created + formatted

---
+++ lib/tile_bag.rb
@@ -0,0 +1,21 @@
+require 'pry'
+require_relative 'scoring.rb'
+require_relative 'player.rb'
+require 'awesome_print'
+
+module Scrabble
+  class TileBag
+    # attr_accessor :plays, :name
+
+    def initialize(name)
+      
+    end
+
+    def draw_tiles(num)
+    end
+
+    def tiles_remaining
+    end
+
+  end
+end
---
+++ specs/tile_bag_spec.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require 'awesome_print'
+require_relative '../lib/tile_bag'
+
+# Get that nice colorized output
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe 'TileBag' do
+  describe 'initialize' do
+    it 'correctly initializes a bag of tiles' do
+      
+    end
+  end
+
+  describe 'draw_tiles' do
+    it '' do
+
+    end
+  end
+
+  describe 'tiles_remaining' do
+    it '' do
+
+    end
+  end
+
+
+end
