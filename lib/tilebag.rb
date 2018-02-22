2803b6a4c1a8f50b5185ec097a170408f09a5845
Tor Shimizu <torshimizu@gmail.com>
2018-02-22 10:56:15 -0800

    created a tilebag class

---
+++ lib/tilebag.rb
@@ -0,0 +1,33 @@
+module Scrabble
+POSSIBLE_LETTERS = {
+  A: 9,
+  N: 6,
+  B: 2,
+  O: 8,
+  C: 2,
+  P: 2,
+  D: 4,
+  Q: 1,
+  E: 12,
+  R: 6,
+  F: 2,
+  S: 4,
+  G: 3,
+  T: 6,
+  H: 2,
+  U: 4,
+  I: 9,
+  V: 2,
+  J: 1,
+  W: 2,
+  K: 1,
+  X: 1,
+  L: 4,
+  Y: 2,
+  M: 2,
+  Z: 1
+}
+  class TileBag
+
+  end
+end
