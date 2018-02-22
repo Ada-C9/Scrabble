c7a1ba6bcbfb8bbb8d98ab9a254150f21f5c7edb
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-02-21 16:57:29 -0800

    Wroted some Player methods. Many tests failing.

--- lib/player.rb
+++ lib/player.rb
@@ -2,28 +2,45 @@ require_relative "scoring.rb"
 
 module Scrabble
   class Player
-    attr_reader :name
+    attr_reader :name, :words_played, :scores
     def initialize(name)
       @name = name.to_s
+      @words_played = []
+      @scores = []
     end
 
     def plays
+      return @words_played
     end
 
     def play(word)
+      if Player.won?
+        return false
+      else
+        @words_played << word
+        word_score = Scoring.score(word)
+        @scores << word_score
+        return word_score
+      end
     end
 
-    def total_score
-    end
+      def total_score
+        return @scores.sum
+      end
 
-    def won?
-    end
+      def won?
+        if Player.total_score > 100
+          return true
+        else
+          return false
+        end
+      end
 
-    def highest_scoring_word
-    end
+      def highest_scoring_word
+      end
 
-    def highest_word_score
-    end
+      def highest_word_score
+      end
 
+    end
   end
-end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -9,7 +9,7 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Player' do
 
-  xdescribe 'initialize' do
+  describe 'initialize' do
 
     it "The new player must be an instance of Player" do
       Scrabble::Player.new("Ana").must_be_instance_of Scrabble::Player
@@ -20,101 +20,113 @@ describe 'Player' do
     end
   end
 
-  xdescribe 'plays' do
+  describe 'plays' do
     it "returns an array" do
-      Scrabble::Player.plays.must_be_kind_of Array
+      player_1 = Scrabble::Player.new("Wini")
+      player_1.plays.must_be_kind_of Array
     end
 
     it "returns an array of words played" do
-
+      player_1 = Scrabble::Player.new("Wini")
+      player_1.play("hello")
+      player_1.play("hi")
+      player_1.plays.must_equal ["hello", "hi"]
     end
   end
 
-  xdescribe 'play' do
+  describe 'play' do
     it "adds a word to plays method" do
-      words_played = Scrabble::Player.plays
-      new_word = "xx"
-      Scrabble::Player.play(new_word)
-      words_played.must_include new_word
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("xx")
+      player_1.words_played.must_include "xx"
     end
 
     it "must return the score of the word if the game is not won" do
-      word = "xx"
-      Scrabble::Player.play(word).must_equal 20
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("xx").must_equal 20
     end
 
     it 'must return false if the game is won' do
-      Scrabble::Player.play("zzzzffd")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("zzzzffd")
       word = "xx"
-      Scrabble::Player.play(word).must_equal false
+      player_1.play(word).must_equal false
     end
 
     it "must return zero if the word played is a blank string" do
+      player_1 = Scrabble::Player.new("Dan")
       word = ""
-      Scrabble::Player.play(word).must_equal 0
+      player_1.play(word).must_equal 0
     end
   end
 
   describe "Total score"  do
-    it "return the the sum score of played words" do
-      score_1 = Scrabble::Player.play("hello")
-      score_2 = Scrabble::Player.play("hi")
-      score_3 = Scrabble::Player.play("hey")
+    it "returns the the sum score of played words" do
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("hello")
+      player_1.play("hi")
+      player_1.play("hey")
 
-      Scrabble::Player.total_score.must_equal 22
+      player_1.total_score.must_equal 22
     end
   end
 
   describe "won?" do
     it "returns true if total score is more than 100" do
-      score_1 = Scrabble::Player.play("zzzzzzz")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("zzzzzzz")
 
-      Scrabble::Player.won?.must_be true
+      player_1.won?.must_be true
     end
 
     it "returns false if total score is equal to 100" do
-      score_1 = Scrabble::Player.play("zzzzffd")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("zzzzffd")
 
-      Scrabble::Player.won?.must_be false
+      player_1.won?.must_be false
     end
 
     it "returns false if total score is less than 100" do
-      score_1 = Scrabble::Player.play("bob")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("bob")
 
-      Scrabble::Player.won?.must_be false
+      player_1.won?.must_be false
     end
   end
 
   describe "highest_scoring_word" do
     it "must return a string" do
-      score_1 = Scrabble::Player.play("hello")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("hello")
 
-      Scrabble::Player.highest_scoring_word.must_be_kind_of String
+      player_1.highest_scoring_word.must_be_kind_of String
     end
 
     it "returns the highest score" do
-      score_1 = Scrabble::Player.play("hello")
-      score_2 = Scrabble::Player.play("hi")
-      score_3 = Scrabble::Player.play("hey")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("hello")
+      player_1.play("hi")
+      player_1.play("hey")
 
-      Scrabble::Player.highest_scoring_word.must equal "hey"
+      player_1.highest_scoring_word.must equal "hey"
     end
   end
 
   describe "highest_word_score" do
     it "must return an integer" do
-      score_1 = Scrabble::Player.play("hello")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("hello")
 
-      Scrabble::Player.highest_word_score.must_be_kind_of Integer
+      player_1.highest_word_score.must_be_kind_of Integer
     end
 
     it "returns the score of the highest scoring word" do
-      score_1 = Scrabble::Player.play("hello")
-      score_2 = Scrabble::Player.play("hi")
-      score_3 = Scrabble::Player.play("hey")
+      player_1 = Scrabble::Player.new("Dan")
+      player_1.play("hello")
+      player_1.play("hi")
+      player_1.play("hey")
 
-      Scrabble::Player.highest_word_score.must_equal 9
+      player_1.highest_word_score.must_equal 9
     end
   end
-
 end
