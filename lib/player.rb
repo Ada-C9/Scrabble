7bd409e07f349377c8e7fcf95e3bbeac85868d38
Aruna <aruna.thungathurthi@gmail.com>
2018-02-22 15:42:28 -0800

    test and code for won? and highest_scoring_word methods

--- lib/player.rb
+++ lib/player.rb
@@ -13,15 +13,30 @@ module Scrabble
 
     def play(word)
       @plays << word
+        if won?
+         return false
+       else
+         return Scrabble::Scoring.score(word)
+       end
     end
 
     def total_score
-    @total = 0
+      @total = 0
 
-    @plays.each do |word|
-      @total += Scrabble::Scoring.score(word)
-    end
+      @plays.each do |word|
+        @total += Scrabble::Scoring.score(word)
+      end
+
       return @total
     end
+
+    def won?
+      @total >= 100 ? (return true) : (return false)
+    end
+
+    def highest_scoring_word
+      return Scrabble::Scoring.highest_score_from(@plays)
+    end
+
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -12,6 +12,7 @@ describe 'Player' do
     @player.play("cat")
     @player.play("rooster")
   end
+
   describe '#initialize method' do
     it "takes a name" do
 
@@ -28,7 +29,7 @@ describe 'Player' do
 
     it "responds to the total_score to 0" do
 
-      @player.must_respond_to :total_score
+      @player.must_respond_to :total
     end
  end
 
@@ -40,6 +41,7 @@ describe 'Player' do
    end
 
     it "adds an input word to plays array" do
+
       @player.play("tiger")
       @player.plays.must_include "tiger"
     end
@@ -49,16 +51,39 @@ describe 'Player' do
       @player.plays.must_equal ["dog","cat","rooster"]
       @player.plays.must_be_kind_of Array
     end
+
+    it "returns false if the player has won" do
+      @player.play("qqqqqqqqqqqq")
+      @player.won?.must_equal false
+    end
+
+    it "returns the score of the word" do
+
+      @player.play("dog").must_equal 5
+    end
   end
 
-  describe '#total_score method' do
+  describe "#total_score method" do
     it "responds to total_score of the played word" do
 
       @player.total_score.must_equal 67
       @player.total_score.must_be_kind_of Integer
     end
-
-
   end
 
+  describe "#won? method" do
+    it "returns true if total_score greater than or equal to 100" do
+
+      @player.play("qqqq")
+      @player.total_score
+      @player.won?.must_equal true
+    end
+  end
+
+  describe "The Highest_score_word" do
+    it "returns the highest scoring played word" do
+
+      @player.highest_scoring_word.must_equal "rooster"
+    end
+  end
 end
