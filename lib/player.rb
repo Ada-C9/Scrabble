cae8fcd16b9e71aaeec3b5c54e6aa350afdbd3c5
Mary Lamkin <mmlamkin@gmail.com>
2018-02-21 16:58:16 -0800

    finished wave 2, tests pass

--- lib/player.rb
+++ lib/player.rb
@@ -5,22 +5,46 @@ require_relative 'scoring'
 module Scrabble
   class Player < Scoring
 
-    attr_reader :name, :plays
+    attr_reader :name, :played_words
+    attr_writer :total_score
 
     def initialize(name)
       @name = name
       @played_words = []
+      @total_score = 0
     end
 
     def play(word)
-      @played_words << word
-      return Scrabble::Scoring.score(word)
+      if @total_score >= 100
+        return false
+      else
+        @played_words << word
+        return Scrabble::Scoring.score(word)
+      end
     end
 
     def total_score
-      
+      scores = @played_words.map { |i| Scrabble::Scoring.score(i) }
+      total_score = scores.reduce(:+)
+      return total_score
     end
 
+    def won?
+      if @total_score >= 100
+        return true
+      else
+        return false
+      end
+    end
+
+    def highest_scoring_word
+      return Scrabble::Scoring.highest_score_from(@played_words)
+    end
+
+    def highest_word_score
+      scores = @played_words.map { |i| Scrabble::Scoring.score(i) }
+      return scores.max
+    end
 
   end
 
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -11,7 +11,7 @@ describe 'Player' do
 
   before do
     @john = Scrabble::Player.new("John")
-    @sally = Scrabble::Player.new("Sally")
+
   end
 
   describe 'initialize' do
@@ -22,8 +22,8 @@ describe 'Player' do
     end
 
     it 'returns an array' do
-      @john.plays.must_be_kind_of Array
-      @john.plays.must_equal []
+      @john.played_words.must_be_kind_of Array
+      @john.played_words.must_equal []
     end
   end
 
@@ -34,11 +34,12 @@ describe 'Player' do
 
     it 'word is added to play array' do
       @john.play("snail")
-      @john.plays.must_include "snail"
+      @john.played_words.must_include "snail"
     end
 
-    xit 'returns false if game is over (player has already won)' do
-
+    it 'returns false if game is over (player has already won)' do
+      @john.total_score = 100
+      @john.play('dog').must_equal false
     end
 
     it 'returns score of word' do
@@ -48,11 +49,41 @@ describe 'Player' do
   end
 
   describe 'total_score' do
-    it 'returns sum of played words'
-    @john.play("board")
-    @john.play("cup")
+    it 'returns sum of played words' do
+      @john.played_words.push("board", "cup")
 
-    @john.total_score.must_equal 15
+      @john.total_score.must_equal 15
+    end
   end
 
+  describe "won?" do
+    it "returns true if player has 100 points" do
+      @john.total_score = 100
+      @john.won?.must_equal true
+    end
+
+    it "returns false if player does not have 100 points" do
+      @john.total_score = 99
+      @john.won?.must_equal false
+    end
+  end
+
+  describe "highest_scoring_word" do
+    it 'returns the highest scoring word' do
+      @john.played_words.push("cat", "zebra")
+      @john.highest_scoring_word.must_equal "zebra"
+    end
+
+    it 'returns score of 7 letter word over other' do
+      @john.played_words.push("aaaaaah", "zzzzzz")
+      @john.highest_scoring_word.must_equal "aaaaaah"
+    end
+  end
+
+  describe "highest_word_score" do
+    it 'returns the highest score played' do
+      @john.played_words.push("cat", "zebra")
+      @john.highest_word_score.must_equal 16
+    end
+  end
 end
