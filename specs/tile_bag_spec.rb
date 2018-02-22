90547c69de03baf6756817ba8a7d3d5e979150c0
Zheng Cao <chori84@gmail.com>
2018-02-22 14:00:21 -0800

    Implement TileBag#draw_tiles and pass tests

--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -9,21 +9,34 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'TileBag' do
 
-  describe 'initialize' do
+  xdescribe 'initialize' do
     it "Set-up the instance with a collection of all default tiles" do
       tile_bag = Scrabble::TileBag.new
 
       tile_bag.must_respond_to :tiles
-      tile_bag.tiles.must_be_instance_of Array    
+      tile_bag.tiles.must_be_instance_of Array
     end
-
   end
 
   describe 'draw_tiles' do
+    it "Returns a collection of random tiles" do
+      tile_bag = Scrabble::TileBag.new
 
+      tile_bag.draw_tiles(7).length.must_equal 7
+      tile_bag.draw_tiles(7).must_be_instance_of Array
+      tile_bag.draw_tiles(7).all?{String}.must_equal true
+      tile_bag.draw_tiles(7).all?{ |l| l.length == 1 }.must_equal true
+    end
+
+    it "Removes the tiles from the default set" do
+      tile_bag = Scrabble::TileBag.new
+      original_tiles = tile_bag.tiles
+
+      (tile_bag.draw_tiles(8) != original_tiles).must_equal true
+    end
   end
 
-  describe 'tiles_remaining' do
+  xdescribe 'tiles_remaining' do
 
   end
 
