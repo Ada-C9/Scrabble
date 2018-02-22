497da2da07808319f9a87dc5dbdb436afa420a25
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-21 19:29:09 -0800

    refactored and finished player wave 3

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,49 @@
+require 'pry'
+
 module Scrabble
   class Player
 
-    attr_reader :name, :plays
+    attr_reader :name, :plays, :total_score, :tiles
 
     def initialize(initial_name)
       @name = provided_valid_name(initial_name)
       @plays = []
+      @total_score = 0
+      @tiles = []
     end
 
     def provided_valid_name(name)
       name = "AnnoyingOne" if name.class != String || name.strip.length == 0
       return name
     end
+
+    def play(word)
+      return false if won?
+      word_score = Scoring.score(word)
+      return if word_score.nil?
+      @plays << word
+      @total_score += word_score
+      return word_score
+    end
+
+    def won?
+      return @total_score >= 100
+    end
+
+    def highest_scoring_word
+      return Scoring.highest_score_from(@plays)
+    end
+
+    def highest_word_score
+      Scoring.score(highest_scoring_word)
+    end
+
+    def draw_tiles(tile_bag)
+      if tile_bag.class != TileBag
+        raise ArgumentError.new("#{tile_bag} must be a TileBag")
+      end
+      @tiles = tile_bag.draw_tiles(MAX_LETTERS - @tiles.size)
+    end
+
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -3,6 +3,7 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative '../lib/player'
+require_relative '../lib/tilebag'
 
 # Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
@@ -38,7 +39,6 @@ describe 'Player' do
     end
   end
 
-# player_words = ["minimal", "BamBam", "Ada", "Loves", "Lace", "Snowone"]
   describe 'plays' do
     it 'return an attribute of a list of player words' do
       name = 'BamBam'
@@ -50,5 +50,160 @@ describe 'Player' do
       player.must_respond_to :plays
     end
   end
+
+  describe 'play(word)' do
+    it 'adds input word to plays array' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      word = "GHOST"
+
+      player.play(word)
+
+      player.plays.include?("GHOST").must_equal true
+    end
+
+    it 'return the score of the provided word' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      word = "ferret"
+
+      word_score = player.play(word)
+
+      word_score.must_equal 9
+    end
+
+    it 'returns false if player has won' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      winning_words = ["glitter", "jittery"]
+
+      winning_words.each { |word| player.play(word) }
+
+      player.play('beam').must_equal false
+    end
+
+    it 'does not add invalid input for word' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      bad_word = nil
+
+      player.play(bad_word)
+
+      player.plays.include?(bad_word).must_equal false
+    end
+
+  end
+
+  describe 'total_score' do
+    it 'returns sum of all players word scores' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      words = ["ghost", "ferret", "litter"]
+
+      words.each { |word| player.play(word) }
+      score_sum = player.total_score
+
+      player.must_respond_to :total_score
+      score_sum.must_equal 24
+    end
+  end
+
+  describe 'won?' do
+    it 'returns true if player has a total score > 100' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      words = ["glitter", "jittery"]
+
+      words.each { |word| player.play(word) }
+
+      player.won?.must_equal true
+    end
+
+    it 'returns true if player has a total score equals 100' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      words = %w[quiz letter dean jittery]
+
+      words.each { |word| player.play(word) }
+
+      player.won?.must_equal true
+    end
+
+    it 'returns false if player has a total score < 100' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+
+      player.play("glitter")
+
+      player.won?.must_equal false
+    end
+
+  end
+
+  describe 'highest_scoring_word' do
+    it 'returns players highest scoring word' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      words = %w[quiz letter dean jittery]
+      words.each { |word| player.play(word) }
+
+      best_word = player.highest_scoring_word
+
+      best_word.must_equal 'jittery'
+    end
+  end
+
+  describe 'highest_word_score' do
+    it 'returns highest scoring word score' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      words = %w[quiz letter dean jittery]
+      words.each { |word| player.play(word) }
+
+      best_word_score = player.highest_word_score
+
+      best_word_score.must_equal 67
+    end
+  end
+
+  describe 'tiles' do
+    it 'has a collection of tile_bag' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+
+      player.must_respond_to :tiles
+      player.tiles.must_be_kind_of Array
+      player.tiles.size.must_equal 0
+    end
+  end
+
+  describe 'draw_tiles' do
+    it 'fill tile bag until it has seven tiles' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      tile_bag = Scrabble::TileBag.new
+      player.draw_tiles(tile_bag)
+
+      player.tiles.size.must_equal 7
+    end
+
+    it 'number of tiles drawn does not exceed 7' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      tile_bag = Scrabble::TileBag.new
+      player.draw_tiles(tile_bag)
+    end
+
+    it 'throws ArgumentError if provided something other than a TileBag' do
+      name = 'BamBam'
+      player = Scrabble::Player.new(name)
+      assert_raises{player.draw_tiles("I am the TileBag")}
+
+    end
+
+  end
+
+
+# "zip", "quill", "quite", "zebra", "quail", "flitter",
 
 end
