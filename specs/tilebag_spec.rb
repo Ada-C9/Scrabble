d0abe56d15f3a5f21e82788541963a5096bcbb26
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-23 14:45:05 -0800

    added test for array size

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -7,14 +7,26 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 
 describe 'TileBag' do
-  describe 'draw_tiles' do
+  describe 'initialize' do
     it 'can be instantiated' do
       tile_bag_test = Scrabble::TileBag.new
-      puts tile_bag_test.tile_bag
+      tile_bag_test.tile_bag.must_be_instance_of Array
 
-      # puts tile_bag_test.draw_tiles(4)
     end
   end
+   xdescribe 'draw_tiles' do
+     it 'can return a random array of characters' do
+       random_arr = Scrabble::TileBag.new
+       array_of_char = random_arr.draw_tiles(4)
+       array_of_char.must_be_instance_of Array
+     end
+
+     it 'can return the size of an array input by the user' do
+       random_arr = Scrabble::TileBag.new
+       arr_of_char = random_arr.draw_tiles(5)
+       arr_of_char.length.must_be_length(5)
+     end
+   end
 end
 
 
