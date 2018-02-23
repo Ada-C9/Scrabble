6582c4ce873a16ce47b9caf855559d3e47fde692
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-22 17:41:24 -0800

    added single test to confirm object could be created

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,7 +1,52 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/tilebag'
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 
-require_relative '../lib/tilebag.rb'
+describe 'TileBag' do
+  describe 'draw_tiles' do
+    it 'can be instantiated' do
+      tile_bag_test = Scrabble::TileBag.new
+      puts tile_bag_test.tile_bag
+
+      # puts tile_bag_test.draw_tiles(4)
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# describe 'TileBag' do
+#
+#   describe 'draw_tiles' do
+#     it 'returns a random number of tiles given by the user and deletes from the tile bag' do
+#       r = ['rrr']
+#       tile_bag_test = Scrabble::TileBag.new(r)
+#       tile_bag_test.draw_tiles(4)
+#       tile_bag_test.must_be_instance_of Array
+#       tile_bag_test.length.must_equal 4
+#     end
+#   end
+# end
+
+  # describe 'plays' do
+  #   it 'returns an Array' do
+  #     player_name = Scrabble::Player.new ("Ada")
+  #     player_name.plays.must_be_instance_of Array
+  #   end
+  #
+  # end
