515363a80e45010b8560ac7d056296cb7a849459
Tor Shimizu <torshimizu@gmail.com>
2018-02-23 12:12:57 -0800

    deleted Player#tilebag

--- lib/player.rb
+++ lib/player.rb
@@ -4,7 +4,6 @@ require 'pry'
 module Scrabble
   class Player
     attr_reader :name, :total_score, :tiles
-    # @@tilebag = Scrabble::TileBag.new
 
     def initialize(name)
       @name = name
@@ -39,16 +38,8 @@ module Scrabble
       return Scrabble::Scoring.score(highest_scoring_word)
     end
 
-    private # adding so user cannot access this method and instantiate a new TileBag
-
-    def self.tilebag
-      return Scrabble::TileBag.new
-    end
-
-    public # want this to be public
-
-    def draw_tiles(tilebag)
-      new_tiles = tilebag.draw_tiles(MAX_WORD_LENGTH - @tiles.length)
+    def draw_tiles(our_tilebag)
+      new_tiles = our_tilebag.draw_tiles(MAX_WORD_LENGTH - @tiles.length)
       new_tiles.each do |tile|
         @tiles << tile
       end
