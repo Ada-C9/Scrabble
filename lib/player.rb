63dbf5b02a72960a381e4430b8a08c12de251db1
Hannah Cameron <hannahiscoding@gmail.com>
2018-02-22 17:20:26 -0800

    adding in lib/player.rb file :)

---
+++ lib/player.rb
@@ -0,0 +1,32 @@
+require 'awesome_print'
+require 'pry'
+
+module Scrabble
+  class Player
+
+    attr_reader :name
+
+    def initialize
+      @name = name
+    end
+
+    def plays
+    end
+
+    def play(word)
+    end
+
+    def total_score
+    end
+
+    def won?
+    end
+
+    def highest_scoring_word
+    end
+
+    def highest_word_score
+    end
+
+  end # End of Player class
+end # End of Scrabble module
