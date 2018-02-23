8283fcffdaef168860ba999d577f8341cfc8884e
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-22 20:51:54 -0800

    added code for tilebag, and tile_bag spec. all tests passing. added player spec file

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,12 +1,51 @@
 module Scrabble
   class TileBag
-#player can see letters
+    #player can see letters
     attr_reader :letter_quantity
 
 
-# Creating an instance of letter quantity to be used though class tilebag.
+    # Creating an instance of letter quantity to be used though class tilebag.
     def initialize
-      @letter_quantity = {A:9, B:2, C:2, D:4, E:12, F:2, G:3, H:2, I:9, J:1, K:1, L:4, M:2, N:6, O:8, P:2, Q:1, R:6, S:4, T:6, U:4, V:2, W:2, X:1, Y:2, Z:1}
+      @letters_bag_total = {A:9, B:2, C:2, D:4, E:12, F:2, G:3, H:2, I:9, J:1, K:1, L:4, M:2, N:6, O:8, P:2, Q:1, R:6, S:4, T:6, U:4, V:2, W:2, X:1, Y:2, Z:1}
     end
+
+    # Define tiles remaining method to be used in the method for drawing the tiles.
+    def tiles_remaining
+      # still not sure why or how this is working. TODO: may take out later
+      return @letters_bag_total.inject(0) { |sum, i| sum += i[1] }
+    end
+    # Define draw tiles, put the tiles into array.
+    def draw_tiles(num)
+      all_tiles = []
+      # array will return tiles to player. Needs much refactoring.
+      if num > tiles_remaining
+        return nil
+        #to account for test, returns nil if more tiles are drawn than tiles remain.
+      else
+        while all_tiles.length != num
+          new_tile = rand(@letters_bag_total.size)
+          i = 0
+          @letters_bag_total.each do |letter, total_tiles|
+            #Need to continue working on, this is becoming harder to read. TODO: REFACTOR!
+            # if the amount of tiles drawn(starting at 0) is the same as the amount of new tiles drawn,
+            if i == new_tile
+              #if the condition above, and the total tiles isnt 0, add the new tile (letter), to all of the tiles (all_tiles array)
+              if total_tiles != 0
+                all_tiles << letter
+                #Then subtract the letter from the tilebag, reducing the total amount of tiles by 1, and reducing the letter by one specifically from the letters.
+                @letters_bag_total[letter] = total_tiles - 1
+              else
+                new_tile = rand(@letters_bag_total.size)
+              end
+            end
+            #increases the amount of tiles had by player plus one, each time a tile is drawn
+            i += 1
+          end
+        end
+        #returns array of all tiles to player
+        return all_tiles
+      end
+    end
+
   end
 end
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -6,7 +6,7 @@ require_relative '../lib/tilebag'
 #With this, i wont have to retype scrabble::tilebag.new so many times.
 describe "tilebag" do
   before do
-   @tile_bag = Scrabble::TileBag.new
+    @tile_bag = Scrabble::TileBag.new
   end
 
   describe "tile bag can be created/initialized" do
@@ -14,4 +14,32 @@ describe "tilebag" do
       @tile_bag.must_be_instance_of Scrabble::TileBag
     end
   end
+  #testing for edge cases of drawing tiles, like drawing more than are in the tilebag, or drawing no tiles.
+  describe "draw tiles" do
+    it "returns nil if input is more than tiles in tilebag" do
+      @tile_bag.draw_tiles(50000).must_be_nil true
+    end
+    #in between case
+    # Making sure drawn tiles will be returned to player
+    it "returns tiles to user in array" do
+      @tile_bag.draw_tiles(5).must_be_kind_of Array
+    end
+    # Testing for an edge case.
+    it "returns an empty array of tiles to user" do
+      @tile_bag.draw_tiles(0).must_be_empty Array
+    end
+  end
+
+  describe "tiles left in tilebag" do
+    it "takes a tile away from total letter quantity and returns num of remaining
+    tiles in tilebag" do
+    @tile_bag.draw_tiles(7)
+    @tile_bag.tiles_remaining.must_equal 91
+  end
+  it "returns the total letter quantity when no tile(s) is drawn" do
+    @tile_bag.draw_tiles(0)
+    @tile_bag.tiles_remaining.must_equal 98
+    # Wont need to test for edge case if all the tiles are drawn with return of 0, because we already did this in previous draw tile test.
+  end
+end
 end
