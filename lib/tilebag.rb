a4067b2e20bbc71b9e8b75ef872eed450f03ffdb
Mary Lamkin <mmlamkin@gmail.com>
2018-02-23 12:13:08 -0800

    adds initialize and draw tiles to tilebag, with tests

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,14 +1,29 @@
-equire 'awesome_print'
+require 'awesome_print'
 require 'pry'
 require_relative 'player'
 
 module Scrabble
   class TileBag
+    attr_accessor :tilebag
 
-    @@bag = { "a" => 9, "b" =>2, "c" => 2, "d" => 4, "e" => 12, "f" => 2, "g" => 3, "h" => 2, "i" => 9, "j" => 1, "k" => 1, "l" => 4, "m" => 2, "n" => 6, "o" => 8, "p" => 2, "q" => 1, "r" => 6, "s" => 4, "t" => 6, "u" => 4, "v" => 2, "w" => 2, "x" => 1, "y" => 2, "z" => 1 }
-    def initialize()
-
+    def initialize
+      @tilebag = { "a" => 9, "b" => 2, "c" => 2, "d" => 4, "e" => 12, "f" => 2, "g" => 3, "h" => 2, "i" => 9, "j" => 1, "k" => 1, "l" => 4, "m" => 2, "n" => 6, "o" => 8, "p" => 2, "q" => 1, "r" => 6, "s" => 4, "t" => 6, "u" => 4, "v" => 2, "w" => 2, "x" => 1, "y" => 2, "z" => 1 }
     end
 
+    def draw_tiles(num)
+      hand = []
+      num.times do
+        letter = @tilebag.keys.sample
+        @tilebag[letter] = @tilebag[letter] - 1
+        if @tilebag[letter] < 1
+          letter = @tilebag.keys.sample
+        end
+
+        hand << letter
+      end
+      return hand
+    end
+
+    def
   end
 end
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,18 +1,38 @@
-equire 'minitest/autorun'
+require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative '../lib/player'
 require_relative '../lib/scoring'
-require_relative '..lib/tilebag'
+require_relative '../lib/tilebag'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'TileBag' do
 
+  before do
+    @bag = Scrabble::TileBag.new
+  end
+
   describe 'initialize' do
-    it '' do
-
-
+    it 'has a collection of all tiles' do
+      new_bag = Scrabble::TileBag.new
+      new_bag.must_be_instance_of Scrabble::TileBag
+      new_bag.tilebag.length.must_equal 26
+      new_bag.tilebag.must_be_kind_of Hash
     end
   end
+
+  describe 'draw_tiles' do
+    it 'returns an array of random tiles' do
+
+      @bag.draw_tiles(3).must_be_kind_of Array
+      @bag.draw_tiles(3)[2].must_be_kind_of String
+    end
+
+    it 'removes drawn tiles from tile bag' do
+      @bag.draw_tiles(3)
+      new_total = @bag.tilebag.values.reduce(:+)
+      new_total.must_equal 96
+  end
+end
