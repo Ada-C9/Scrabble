d2ab43616da94d90767ff285ed21e25f6c77af4b
Kate Pond <kate@thepondsedge.com>
2018-02-23 14:56:13 -0800

    refactors tilebag.rb

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -5,46 +5,45 @@ module Scrabble
     attr_reader :tiles, :drawn_tiles
 
     def initialize
-      @tiles = %w[
-        a a a a a a a a a
-        b b
-        c c
-        d d d d
-        e e e e e e e e e e e e
-        f f
-        g g g
-        h h
-        i i i i i i i i i
-        j
-        k
-        l l l l
-        m m
-        n n n n n n
-        o o o o o o o o
-        p p
-        q
-        r r r r r r
-        s s s s
-        t t t t t t
-        u u u u
-        v v
-        w w
-        x
-        y y
-        z
-      ]
-    end
+      tiles_hash = {
+        "a" => 9,
+        "b" => 2,
+        "c" => 2,
+        "d" => 4,
+        "e" => 12,
+        "f" => 2,
+        "g" => 3,
+        "h" => 2,
+        "i" => 9,
+        "j" => 1,
+        "k" => 1,
+        "l" => 4,
+        "m" => 2,
+        "n" => 6,
+        "o" => 8,
+        "p" => 2,
+        "q" => 1,
+        "r" => 6,
+        "s" => 4,
+        "t" => 6,
+        "u" => 4,
+        "v" => 2,
+        "w" => 2,
+        "x" => 1,
+        "y" => 2,
+        "z" => 1
+      }
 
-    def draw_tiles(number)
-      drawn_tiles_array = Array.new
-      number.times do
-        index = rand(@tiles.length)
-        drawn_tiles_array << @tiles[index]
-        @tiles.delete_at(index)
+      @tiles = []
+      tiles_hash.each do |letter, quanity|
+        letters = (1..quanity).collect { letter } # creates an array of the number of letters e.g ["b","b"]
+
+        @tiles << letters # creates an array of an array
       end
+      @tiles = @tiles.flatten # makes the multidimensional array into a single array
 
-        return drawn_tiles_array
-
+      return @tiles
+    end
 
     def draw_tiles(num)
       @drawn_tiles = @tiles.sample(num)
