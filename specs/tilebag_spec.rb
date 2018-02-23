10be9d4bc8d13449eab54b8d7db49899ed785b7d
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-02-22 16:34:14 -0800

    add require_relative file

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -2,3 +2,6 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+
+require_relative '../lib/tilebag.rb'
