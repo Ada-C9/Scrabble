ffc2370b577d1ac2c379b1677a94428eddba8017
Madaleine Shields <maddiev.shields@gmail.com>
2018-02-22 18:39:20 -0800

    initiate payer.rb

---
+++ player.rb
@@ -0,0 +1,30 @@
+require_relative "scoring"
+
+# The constructor for Scrabble::Player should take exactly one argument: the player's name. Instances of the class should respond to the following messages:
+#
+# #play(word): Adds the input word to the plays Array
+# Returns false if player has already won
+# Otherwise returns the score of the word
+
+# #total_score: Returns the sum of scores of played words
+# #won?: If the player has over 100 points, returns true, otherwise returns false
+# #highest_scoring_word: Returns the highest scoring played word
+# #highest_word_score: Returns the highest_scoring_word score
+
+module Scrabble
+  class Player
+
+    def initialize(name)
+       @name = name
+       # #name: returns the value of the @name instance variable
+
+       @plays = []
+       # #plays: returns an Array of the words played by the player
+
+       @total_score = 0
+       @tiles=[]
+
+     end
+
+  end
+end
