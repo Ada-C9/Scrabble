76f919a6d2421b8d6b3dd4669f53f7f56a961c8a
Hannah Cameron <hannahiscoding@gmail.com>
2018-02-22 17:18:36 -0800

    built stub methods for all wave2, wrote first test

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -6,3 +6,12 @@ require_relative '../lib/scoring'
 
 # Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe 'Player' do
+  describe 'plays' do
+
+    it 'correctly returns an array of the words played by the player' do
+      Scrabble::Player.plays.must_be Array
+    end
+  end
+end
