5f37066289bb1ff93ac67f27aef24a75e0125325
Maja Graonic <m.graonic@gmail.com>
2018-02-22 18:19:20 -0800

    started TileBag class

---
+++ lib/tilebag.rb
@@ -0,0 +1,18 @@
+
+
+module Scrabble
+  class TileBag
+    def initialize
+
+      @tiles_remaining =
+    end
+
+    def draw_tiles
+      # returns a collection of random tiles
+      # removes the tiles from default_tiles
+    end
+
+
+
+  end
+end
