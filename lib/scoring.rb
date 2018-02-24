819ceffeea5aaab5c2965122c68c5e569796f051
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-23 23:41:14 -0800

    added comments and cleaned up formatting

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,6 +1,7 @@
 module Scrabble
   class Scoring
 
+    # Returns total score of a given word.
     def self.score(word)
       total = 0
       array_of_characters = word.split('')
@@ -10,23 +11,23 @@ module Scrabble
         array_of_characters.each do |letter|
           scrabble_value = letter.upcase
           case scrabble_value
-            when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
-              scrabble_value = 1
-            when "D", "G"
-              scrabble_value = 2
-            when "B", "C", "M", "P"
-              scrabble_value = 3
-            when "F", "H", "V", "W", "Y"
-              scrabble_value = 4
-            when "K"
-              scrabble_value = 5
-            when "J","X"
-              scrabble_value = 8
-            when "Q", "Z"
-              scrabble_value = 10
-            end
-            total = scrabble_value + total
+          when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
+            scrabble_value = 1
+          when "D", "G"
+            scrabble_value = 2
+          when "B", "C", "M", "P"
+            scrabble_value = 3
+          when "F", "H", "V", "W", "Y"
+            scrabble_value = 4
+          when "K"
+            scrabble_value = 5
+          when "J","X"
+            scrabble_value = 8
+          when "Q", "Z"
+            scrabble_value = 10
           end
+          total = scrabble_value + total
+        end
       end
       if array_of_characters.length == 7
         total = total + 50
@@ -34,10 +35,9 @@ module Scrabble
       return total
     end
 
+    # Returns highest score from an array of words.
     def self.highest_score_from(array_of_words)
       max = 0
-      #winner = nil
-      #short = 0
       if array_of_words.size == 0
         highest_score = nil
       elsif array_of_words.size == 1
