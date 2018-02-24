bddee42456d8451ca2d762ea331c86cb451a5a08
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-23 23:43:10 -0800

    Added test for a full plaque and cleaned formatting

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
@@ -16,21 +17,32 @@ describe 'Player' do
   end
 
   describe 'tiles' do
-    it 'is a collection of letters that the player can play (max 7)' do
-      # player = Scrabble::Player.new ("Ada")
-      # player.tiles.must_be_instance_of Array
-      # player.tiles.length.must_equal 0
-      ##player_name.tiles.length.must_be :<, 8
+    it 'is a collection of letters that the player can play (max 7) and starts at 0' do
+      player = Scrabble::Player.new ("Ada")
+      player.tiles.must_be_instance_of Array
+      player.tiles.length.must_equal 0
     end
+
   end
 
   describe 'draw_tiles' do
-   it 'fills tiles array until it has 7 letters from the given tile bag' do
-     # player = Scrabble::Player.new ("Ada")
-     # tile_bag_test = Scrabble::TileBag.new
-     # player.draw_tiles(tile_bag_test)
-     # player.tiles.length.must_equal 7
-   end
+    it 'Fills a partially populated plaque' do
+      player_name = Scrabble::Player.new("Ada")
+      tile_bag = Scrabble::TileBag.new
+      player_name.players_plaque = ['C','D','E']
+      player_name.draw_tiles(tile_bag)
+      player_name.tiles.length.must_equal 7
+    end
+
+    it 'Will not fill in a fully populated plaque' do
+      player_name = Scrabble::Player.new("Ada")
+      tile_bag = Scrabble::TileBag.new
+      testing_arr = ['A','B','C','D','E','F','G']
+      player_name.players_plaque = testing_arr
+      player_name.draw_tiles(tile_bag)
+      player_name.players_plaque.must_equal(testing_arr)
+    end
+
   end
 
   describe 'plays' do
@@ -45,20 +57,17 @@ describe 'Player' do
     it 'adds the word into the array of words' do
       player_name = Scrabble::Player.new ("Ada")
       player_name.play("pop")
-
       player_name.plays.must_include "pop"
     end
 
     it 'Returns false if player has already won' do
       player_name = Scrabble::Player.new ("Ada")
       player_name.play("ZZZZZZZ")
-      ####
       must_be_false = player_name.play("POP")
       must_be_false.must_equal false
-
     end
 
-    it 'Otherwise returns the score of the word' do
+    it 'Returns score of word if < 100 ' do
       player_name = Scrabble::Player.new ("Ada")
       must_return_word = player_name.play("lawl")
       must_return_word.must_equal 7
@@ -92,41 +101,26 @@ describe 'Player' do
     end
 
   end
-  #
+
   describe 'highest_scoring_word' do
     it 'Returns highest scoring word in an array of words for a single player' do
       player_name = Scrabble::Player.new("Ada")
       player_name.play('hi')
       player_name.play('bye')
-      player_name.play('cry')
       player_name.play('why')
+      player_name.play('cry')
 
       player_name.highest_scoring_word.must_equal 'why'
     end
   end
-
-  describe 'Tile-' do
-    it 'Returns highest scoring word in an array of words for a single player' do
-      player_name = Scrabble::Player.new("Ada")
-      tile_bag = Scrabble::TileBag.new
-      player_name.players_plaque = ['C','D','E']
-      player_name.draw_tiles(tile_bag)
-      player_name.tiles.length.must_equal 7
-      puts player_name.tiles
-
-    end
-  end
-
-
-
 
   describe 'highest_word_score' do
     it "Returns the score of the highest scoring word" do
       player_name = Scrabble::Player.new("Ada")
       player_name.play('hi')
       player_name.play('bye')
-      player_name.play('cry')
       player_name.play('ZZZZZZZ')
+      player_name.play('cry')
 
       player_name.highest_word_score.must_equal 120
     end
