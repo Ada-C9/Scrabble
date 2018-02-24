7d0b694b613c91e850645d659f853ede096d705f
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-23 23:47:41 -0800

    Included tests for tilebag and draw tiles

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/tilebag'
@@ -18,7 +19,7 @@ describe 'TileBag' do
       random_arr = Scrabble::TileBag.new
       array_of_char = random_arr.draw_tiles(4)
       array_of_char.must_be_instance_of Array
-      array_of_char.length.must_equal 4 # Add to git
+      array_of_char.length.must_equal 4
     end
 
 
@@ -28,7 +29,15 @@ describe 'TileBag' do
       random_tiles.must_be_instance_of Array
       random_tiles.length.must_equal 0
     end
-    #
+
+    it 'returns an empty array of tiles when given a number less than 1' do
+      tile_bag_test = Scrabble::TileBag.new
+      random_tiles = tile_bag_test.draw_tiles(0)
+      random_tiles.must_be_instance_of Array
+      random_tiles.length.must_equal 0
+    end
+
+
     it 'deletes the tiles from the tile bag' do
       tile_bag_test = Scrabble::TileBag.new
       random_tiles = tile_bag_test.draw_tiles(4)
@@ -42,14 +51,6 @@ describe 'TileBag' do
       tile_bag_test = Scrabble::TileBag.new
       tile_bag_test.tiles_remaining.must_equal 98
     end
-
-    it "returns the number of tiles remaining in a used tile bag " do
-      tile_bag_test = Scrabble::TileBag.new
-      tile_bag_test.draw_tiles(4)
-      tile_bag_test.tiles_remaining.must_equal 94
-    end
-
-
 
   end
 end
