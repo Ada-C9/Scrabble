f2b3ac46925bd7e81e1a30b448d77896c311ef69
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-23 23:40:34 -0800

    cleaned up comments

--- lib/player.rb
+++ lib/player.rb
@@ -1,34 +1,38 @@
 require 'awesome_print'
 MAX_NUM_OF_TILES_ALLOWED = 7
+
 module Scrabble
   class Player
-
-    attr_reader :name
+    attr_reader :name, :players_plaque
     attr_writer :players_plaque
+
     def initialize(name)
-      #returns the value of the @name instance variable
       @name = name
       @array_of_words = []
       @players_plaque = []
     end
 
+    # Returns array of players tiles.
     def tiles
       return @players_plaque
     end
 
+    # Draws tiles to fill player's plaque.
     def draw_tiles(title_bag)
-      tile_bag = Scrabble::TileBag.new
-      add_tiles = MAX_NUM_OF_TILES_ALLOWED  - @players_plaque.length
-      @players_plaque.concat(tile_bag.draw_tiles(add_tiles))
+      if @players_plaque.length < MAX_NUM_OF_TILES_ALLOWED
+        tile_bag = Scrabble::TileBag.new
+        add_tiles = MAX_NUM_OF_TILES_ALLOWED  - @players_plaque.length
+        @players_plaque.concat(tile_bag.draw_tiles(add_tiles))
+      end
     end
 
+    # Returns an array of the words played by the user.
     def plays
-      #returns an Array of the words played by the player
       return @array_of_words
     end
 
+    # Adds the input word to the plays array
     def play(word)
-      #Adds the input word to the plays Array
       player_status = false
       total_score = 0
       @array_of_words.each do |word_in_arr|
@@ -44,10 +48,8 @@ module Scrabble
       return player_status
     end
 
-
+    # Returns the sum of scores of played words
     def total_score
-      #Returns the sum of scores of played words
-      #Look for an enum update below
       sum = 0
       @array_of_words.each do |a_word|
         sum+= Scrabble::Scoring.score(a_word)
@@ -55,27 +57,25 @@ module Scrabble
       return sum
     end
 
-
+    # If the player has over 100 points, returns true, otherwise returns false
     def won?
-       #If the player has over 100 points, returns true, otherwise returns false
-       player_case = false
-       if total_score >= 100
-         player_case = true
-       end
-       return player_case
+      player_case = false
+      if total_score >= 100
+        player_case = true
+      end
+      return player_case
     end
 
+    # Returns the highest scoring played word
     def highest_scoring_word
-      #Returns the highest scoring played word
       highest_scoring_word = Scrabble::Scoring.highest_score_from(@array_of_words)
 
       return highest_scoring_word
     end
 
+    # Returns the highest_scoring_word score
     def highest_word_score
-      #Returns the highest_scoring_word score
       return highest_scoring_value = Scrabble::Scoring.score(highest_scoring_word)
-
     end
 
   end
