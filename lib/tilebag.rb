a32018dd0307c33e56ad28d153b15b4377e515e4
amanzan1 <anna.manzanares7683@gmail.com>
2018-02-25 10:42:21 -0800

    Finished last two methods for tilebag.

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -8,25 +8,43 @@ module Scrabble
     attr_accessor :starting_tiles, :hand_of_tiles
 
     def initialize
-      @hand_of_tiles = []
+
+      @remaining_tiles = []
       @starting_tiles = {
         A: 9, B: 2, C: 2, D: 4, E: 12, F: 2,
         G: 3, H: 2, I: 9, J: 1, K: 1, L: 4,
         M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4,
         T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
       }
-      return @starting_tiles
+
+      @starting_tiles.each do |letter,value|
+
+        # letter.to_s
+        value.times do
+          @remaining_tiles.push(letters.to_s)
+        end
+      end
+
+      @remaining_tiles = @remaining_tiles.shuffle
+
     end
 
     def draw_tiles(number)
-      number.times do
-        tile = @starting_tiles.keys.sample
-        tile = tile.to_s
-        @hand_of_tiles.push(tile)
+       return @remaining_tiles.shift(number)
+    end
 
-        @starting_tiles[tile.to_sym] -= 1
-      end
-      return @hand_of_tiles
+    def tiles_remaining(number)
+
+    end
+
+
+      #   tile = @starting_tiles.keys.sample
+      #   tile = tile.to_s
+      #   @hand_of_tiles.push(tile)
+      #
+      #   @starting_tiles[tile.to_sym] -= 1
+      # end
+      # return @hand_of_tiles
     end
 
 
