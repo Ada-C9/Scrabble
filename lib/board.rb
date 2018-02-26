ba9dadd27562c4f79cd01bcf5d3dc5b9926d78a5
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-25 16:07:03 -0800

    working randomness. *last commit before changing row coordinate to letters

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,10 @@
+# QUESTION! how defensive do I need to be no my private methods?
 module Scrabble
   class Board
 
     attr_reader :size
 
-    def initialize(dimensions = 16 )
+    def initialize(dimensions = 16)
       @size = 0
       @board = create_board(dimensions)
     end
@@ -17,30 +18,29 @@ module Scrabble
           row = "#{row}| #{element} "
         end
         board_as_string << "#{row}|\n#{"----" * @size}"
-        # board_as_string = "#{board_as_string}#{row}|\n#{"----" * @board.length}"
       end
       return board_as_string
     end
 
-    def add_word(coordinates, word) #test if not a word
-      return if !is_valid_word?(word)
-      coordinates = convert_to_indices_if_valid(coordinates)
-      return if coordinates.nil?
-      return fit_if_valid(coordinates, word)
-      # fits_down?(coordinates, word)
+    def add_word(row, col, word) #test if not a word
+      valid_word_or_error(word)
+      valid_coordinates_or_error(row, col)
+      return fit_if_valid(row, col, word.upcase)
     end
 
     private
 
-    def is_valid_word?(word)
-      return word.class == String && word.length > 0 && !word.upcase.match?(/[^A-Z]+?/)
+    def valid_word_or_error(word) # TODO: test for downcase ok
+      if word.class != String || word.length < 1 || word.upcase.match?(/[^A-Z]+?/)
+        raise ArgumentError.new("#{word} must be a String with only letters.")
+      end
     end
 
-    def fit_if_valid(coordinates, word)
-      row, col = coordinates[0..1]
+    def fit_if_valid(row, col, word)
+      row -= 1
+      col -= 1
       if col + word.length <= @size && row + word.length <= @size
-        puts "hi"
-        return insert_down_or_across(row, col, word)
+        return insert_across_or_down(row, col, word)
       elsif  col + word.length <= @size
         return insert_across(row, col, word)
       elsif row + word.length <= @size
@@ -48,8 +48,9 @@ module Scrabble
       end
     end
 
-    def insert_down_or_across(row, col, word)
-      if rand(0..1) == 0
+
+    def insert_across_or_down(row, col, word)
+      if rand(0..1) == 0 # Randomly picks a place to start
         return word if !insert_across(row, col, word).nil?
         return word if !insert_down(row, col, word).nil?
       else
@@ -59,35 +60,41 @@ module Scrabble
     end
 
     def insert_across(row, col, word)
-      @board[row][col...word.length].each_with_index do |element, index|
-        return if !element.nil? && element != word[index]
-      end
+      return if !has_space_across_for_word?(row, col, word)
       (0...word.length).each { |index| @board[row][col + index] = word[index] }
       return word
     end
 
-    def insert_down(row, col, word)
-      index = 0
-      while index < word.length
-      # @board[row...word.length][col].each_with_index do |element, index|
-        return if !@board[row + index][col].nil? && @board[row + index][col] != word[index]
-        index += 1
+    def has_space_across_for_word?(row, col, word)
+      @board[row][col...word.length].each_with_index do |board_space, index|
+        return if !board_space.nil? && board_space != word[index]
       end
-      (0...word.length).each { |index2| @board[row + index2][col] = word[index2] }
       return word
     end
 
-    def convert_to_indices_if_valid(coordinates)
-      return if coordinates.class != Array || coordinates.size != 2
-      coordinates.each_with_index do |coordinate, index|
-        return if !is_valid_coordinate?(coordinate)
-        coordinates[index] -= 1
-      end
-      return coordinates
+    def insert_down(row, col, word)
+      return if !has_space_down_for_word?(row, col, word)
+      (0...word.length).each { |index| @board[row + index][col] = word[index] }
+      return word
     end
 
+    def has_space_down_for_word?(row, col, word)
+      word.length.times do |index|
+        board_space = @board[row + index][col]
+        return if !board_space.nil? && board_space != word[index]
+      end
+      return word
+    end
+
+    #
     def is_valid_coordinate?(coordinate)
       return coordinate.class == Integer && coordinate.between?(1, @size)
+    end
+
+    def valid_coordinates_or_error(row, column)
+      if [row, column].any? { |coordinate| !is_valid_coordinate?(coordinate) }
+        raise ArgumentError.new("#{coordinate} must be an int between 1-#{@size}")
+      end
     end
 
     # Throws ArgumentError if provided dimensions are not a positive number.
@@ -100,8 +107,8 @@ module Scrabble
 
     # Throws ArgumentError if provided dimensions are not a positive number.
     def check_initial_dimension(dimensions)
-      if dimensions.class != Integer || dimensions < 1
-        raise ArgumentError.new("Initial dimensions must be a positive number.")
+      if dimensions.class != Integer || !dimensions.between?(1, 100) # TODO Test for this
+        raise ArgumentError.new("Initial dimensions must be 1 and 100.")
       end
     end
 
