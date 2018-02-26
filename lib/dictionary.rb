7c3f5b23c4f6a0d3aa0020f56f53a6101006bdaa
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-26 00:59:54 -0800

    final working dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,36 +1,42 @@
+# Kaitlin Forsman and Kirsten Schumy
+# Ada Cohort[9], Ampers
+# Scrabble Assignment
+
+# Used Scrabble::DictionaryBuilder (lib/dictionary_files_builder.rb) to create
+# the file (support/all_words.csv) the list is pulled from. All the words in
+# all_words.csv should contain 1 - 7 letters, but this program checks anyways.
+
 require 'csv'
-require 'awesome_print'
-
-
-# Create a `Scrabble::Dictionary` class that includes a method (class or
-# instance) for searching a list of words to determine if a given word is
-# valid (__must have tests__).
 
 module Scrabble
   class Dictionary
 
     @@all_words = []
 
+    # Throws ArgumentError if provided word is not a String.
+    # Returns 'true' if word is in the dictionary and 'false' otherwise.
     def self.is_word?(word)
       raise ArgumentError.new("Invalid input: #{word}") if word.class != String
       build_all_words if @@all_words.empty?
-      return @@all_words.include?(word)
+      return @@all_words.include?(word.upcase)
+    end
+
+    # Returns all the words in the dictionary.
+    def self.get_all_words
+      build_all_words if @@all_words.empty?
+      return @@all_words
     end
 
     private
 
+    # Populates @@all_words with word less than MAX_LETTERS in length.
     def self.build_all_words
-      words = CSV.read('support/all_words.csv')
-      # @@all_words << words.split(",")
-      # puts words[0..3].inspect
-      words[0].each { |word| @@all_words << word}
-    #   @@all_words = words.to_h
-      # ap @@all_words
+      # Based on the version of Scrabble::DictionaryBuilder as of (2/26/18),
+      # which created files with all the words in one row.
+      CSV.read('support/all_words.csv').each do |row|
+        row.each { |word| @@all_words << word if word.length.between?(1, 7) }
+      end
     end
 
   end
 end
-
-
-# puts Scrabble::Dictionary.is_word?("qi")
-# puts Scrabble::Dictionary.is_word?("hello")
