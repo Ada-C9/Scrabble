986c9f386bdc60491b3f0862d5fa771f9076291b
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-25 22:00:18 -0800

    added additional draw_tiles tests

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,31 +1,44 @@
+# Kaitlin Forsman and Kirsten Schumy
+# Ada Cohort[9], Ampers
+# Scrabble Assignment
+
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative '../lib/tilebag'
 
-# Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'TileBag' do
+
   describe 'initialize' do
     it 'sets up an instance of default tiles' do
       tile_bag = Scrabble::TileBag.new
-
       tile_bag.must_be_kind_of Scrabble::TileBag
-      tile_bag.initial_bag.must_be_kind_of Array
-      tile_bag.initial_bag.size.must_equal 98
-      # TODO make sure all the values are int and positive
+      tile_bag.must_respond_to :tiles_in_bag
+      tile_bag.tiles_in_bag.must_be_kind_of Array
+      tile_bag.tiles_in_bag.size.must_equal 98
+    end
+
+    it 'only includes letters' do
+      tile_bag = Scrabble::TileBag.new
+      tiles_from_bag = tile_bag.tiles_in_bag
+      tiles_from_bag.all?(String).must_equal true
+      tiles_from_bag.all? { |letter| letter.size == 1 }.must_equal true
+      tiles_from_bag.all? { |letter| letter.match?(/[A-Z]/) }.must_equal true
+    end
+
+    it 'set up tiles_remaining at initialize' do
+      tile_bag = Scrabble::TileBag.new
+      tile_bag.tiles_remaining.must_equal 98
     end
   end
 
   describe 'draw_tiles(num)' do
     it 'returns a collection of random tile' do
       tile_bag = Scrabble::TileBag.new
-      num = 5
-
-      draw = tile_bag.draw_tiles(num)
-
+      draw = tile_bag.draw_tiles(5)
       draw.length.must_equal 5
       draw.must_be_kind_of Array
       assert draw.all? { |letter| letter.class == String }
@@ -33,42 +46,43 @@ describe 'TileBag' do
 
     it 'removes tiles from default set' do
       tile_bag = Scrabble::TileBag.new
-      num = 5
       size_before = 98
-      tile_bag.draw_tiles(num)
-
-      tile_bag.tiles_remaining.must_equal size_before - num
+      tile_bag.draw_tiles(5)
+      tile_bag.tiles_remaining.must_equal size_before - 5
     end
 
-    it 'must draw number of tiles between 0 and 8 (exclusive)' do
+    it 'returns nil if requested number of tiles is < 1 or > 7' do
       tile_bag = Scrabble::TileBag.new
-
-      draw_1 = tile_bag.draw_tiles(0)
-      draw_2 = tile_bag.draw_tiles(8)
-
-      draw_1.must_be_nil
-      draw_2.must_be_nil
+      tile_bag.draw_tiles(0).must_be_nil
+      tile_bag.draw_tiles(8).must_be_nil
     end
 
+    it 'returns nil if requested number of tiles is not an int' do
+      tile_bag = Scrabble::TileBag.new
+      tile_bag.draw_tiles("Gimme tiles").must_be_nil
+    end
 
     it 'does not draw unavailable letter' do
       tile_bag = Scrabble::TileBag.new
-
-      14.times do
-        tile_bag.draw_tiles(7)
-      end
-
+      14.times { tile_bag.draw_tiles(7) }
+      tile_bag.tiles_remaining.must_equal 0
       tile_bag.draw_tiles(1).must_be_nil
     end
 
+    it 'does not draw tiles beyond the amount available' do
+      tile_bag = Scrabble::TileBag.new
+      13.times { tile_bag.draw_tiles(7) }
+      tile_bag.draw_tiles(5)
+      tile_bag.tiles_remaining.must_equal 2
+      tile_bag.draw_tiles(3).size.must_equal 2
+      tile_bag.tiles_remaining.must_equal 0
+    end
   end
 
   describe 'tiles_remaining' do
     it 'returns how many tiles are left' do
       tile_bag = Scrabble::TileBag.new
-
       tile_bag.tiles_remaining.must_equal 98
-
       tile_bag.draw_tiles(5)
       tile_bag.tiles_remaining.must_equal 93
     end
