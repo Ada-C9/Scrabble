80e406fe23b15d720c52413a751b20a33e285421
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-25 22:47:30 -0800

    changed tests so row was letter and not number after raking about hundred times

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -2,15 +2,12 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
-# require_relative '../lib/player'
 require_relative '../lib/board'
 
-# Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'board' do
   describe 'initialize' do
-    # Creates a new board
     it 'creates a board' do
       test_board = Scrabble::Board.new()
       test_board.must_be_kind_of Scrabble::Board #attr_reader test needed!
@@ -31,39 +28,42 @@ describe 'board' do
       row =  "\n#{"|   " * 16}|\n"
       whole_board = "#{"#{line}#{row}" * 16}#{line}"
 
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-      # ----------------------------------------------------------------
-      # |   |   |   |   |   |   |   |   |   |   | M | A | G | G | I | E |
-      # ----------------------------------------------------------------
+      # Board does not print with letters and numbers
+      #
+      #     1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16
+      #   ----------------------------------------------------------------
+      # A |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # B |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # C |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # D |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # E |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # F |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # G |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # H |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # I |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # J |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # K |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # L |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # M |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # N |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # O |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
+      # P |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+      #   ----------------------------------------------------------------
 
       test_board.print_board.must_equal whole_board
     end
@@ -79,7 +79,7 @@ describe 'board' do
 
     it 'adds letters correctly' do
       test_board = Scrabble::Board.new(4)
-      test_board.add_word(1, 1, "DOG")
+      test_board.add_word("A", 1, "DOG")
       line = "----------------"
       row = "\n|   |   |   |   |\n"
       set_a = "#{line}#{row}"
@@ -130,7 +130,7 @@ describe 'board' do
       while attempt_count < 10_000
         attempt_count += 1
         test_board = Scrabble::Board.new(4)
-        test_board.add_word(1, 1, "DOG")
+        test_board.add_word("A", 1, "DOG")
 
         board_as_string = test_board.print_board
         if board_as_string == across
@@ -152,7 +152,7 @@ describe 'board' do
     it "adds a word to the board and returns 'true'" do
       word = "MAGGIE"
       test_board = Scrabble::Board.new()
-      test_board.add_word(5, 3, "MAGGIE").must_equal word
+      test_board.add_word("E", 3, "MAGGIE").must_equal word
     end
 
     it "returns 'word' if is forced along board edge" do
@@ -191,57 +191,57 @@ describe 'board' do
       # |   |   |   |   |   |   |   |   |   |   | M | A | G | G | I | E |
       # ----------------------------------------------------------------
       expected_board_end =  "| M | A | G | G | I | E |\n#{"----" * 16}"
-      test_board.add_word(16, 11, "MAGGIE").must_equal "MAGGIE"
+      test_board.add_word("P", 11, "MAGGIE").must_equal "MAGGIE"
 
       test_board.print_board.slice!(-90..-1).must_equal expected_board_end
 
-      test_board.add_word(1, 16, "HELLO").must_equal "HELLO"
+      test_board.add_word("A", 16, "HELLO").must_equal "HELLO"
     end
 
     it "returns 'word' if is forced into a valid position" do
       test_board = Scrabble::Board.new()
       word = "PUDDING"
-      test_board.add_word(5, 3, "MAGGIE")
-      test_board.add_word(5, 1, word).must_equal word
+      test_board.add_word("E", 3, "MAGGIE")
+      test_board.add_word("E", 1, word).must_equal word
     end
 
     it "returns 'word' if is forced to overlap in valid position" do
       test_board = Scrabble::Board.new()
       word = "PUDDING"
-      test_board.add_word(16, 11, "MAGGIE").must_equal "MAGGIE"
-      test_board.add_word(10, 14, word).must_equal word
+      test_board.add_word("P", 11, "MAGGIE").must_equal "MAGGIE"
+      test_board.add_word("J", 14, word).must_equal word
     end
 
     it "returns 'nil' if provided coordinate outside range'" do
       test_board = Scrabble::Board.new()
-      assert_raises{test_board.add_word(99, 3, "MAGGIE")}
-      assert_raises{ test_board.add_word(3, 99, "MAGGIE")}
-      assert_raises{ test_board.add_word(3, -1, "MAGGIE")}
-      assert_raises{ test_board.add_word(-1, 12, "MAGGIE")}
-      assert_raises{ test_board.add_word(0, 3, "MAGGIE")}
-      assert_raises{ test_board.add_word(3, 0, "MAGGIE")}
+      assert_raises{test_board.add_word("FOO", 3, "MAGGIE")}
+      assert_raises{ test_board.add_word("C", 99, "MAGGIE")}
+      assert_raises{ test_board.add_word("C", -1, "MAGGIE")}
+      assert_raises{ test_board.add_word("", 12, "MAGGIE")}
+      assert_raises{ test_board.add_word(3, 3, "MAGGIE")}
+      assert_raises{ test_board.add_word("C", 0, "MAGGIE")}
     end
 
     it "returns 'nil' if word is not a string'" do
       test_board = Scrabble::Board.new()
-      assert_raises{ test_board.add_word(5, 3, :maggie)}
+      assert_raises{ test_board.add_word("E", 3, :maggie)}
     end
 
     it "returns 'nil' if word is empty'" do
       test_board = Scrabble::Board.new()
-      assert_raises{ test_board.add_word(5, 3, "") }
+      assert_raises{ test_board.add_word("E", 3, "") }
     end
 
     it "returns 'nil' if word is contains any spaces'" do
       test_board = Scrabble::Board.new()
-      assert_raises{ test_board.add_word(5, 3, "   ")}
-      assert_raises{ test_board.add_word(2, 3, "HELLO MAGGIE") }
+      assert_raises{ test_board.add_word("E", 3, "   ")}
+      assert_raises{ test_board.add_word("B", 3, "HELLO MAGGIE") }
     end
 
     it "returns 'nil' if word is contains any non-letters'" do
       test_board = Scrabble::Board.new()
-      assert_raises{ test_board.add_word(2, 3, "HELLO,MAGGIE") }
-      assert_raises{ test_board.add_word(2, 3, "M4GG1E") }
+      assert_raises{ test_board.add_word("B", 3, "HELLO,MAGGIE") }
+      assert_raises{ test_board.add_word("B", 3, "M4GG1E") }
     end
 
 
