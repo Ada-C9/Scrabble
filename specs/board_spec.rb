4518cd97568550f074092ae97a794914e1087792
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-25 16:06:20 -0800

    added test for randomness. *last commit before changing row coordinate to letters

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -13,7 +13,7 @@ describe 'board' do
     # Creates a new board
     it 'creates a board' do
       test_board = Scrabble::Board.new()
-      test_board.must_be_kind_of Scrabble::Board
+      test_board.must_be_kind_of Scrabble::Board #attr_reader test needed!
     end
 
     it 'only creates a board if the values are integers > 0' do
@@ -79,14 +79,14 @@ describe 'board' do
 
     it 'adds letters correctly' do
       test_board = Scrabble::Board.new(4)
-      test_board.add_word([1, 1], "CAT")
+      test_board.add_word(1, 1, "DOG")
       line = "----------------"
       row = "\n|   |   |   |   |\n"
       set_a = "#{line}#{row}"
       set_d = "|   |   |   |\n#{line}\n"
 
       # ----------------
-      # | C | A | T |   |
+      # | D | O | G |   |
       # ----------------
       # |   |   |   |   |
       # ----------------
@@ -94,31 +94,65 @@ describe 'board' do
       # ----------------
       # |   |   |   |   |
       # ----------------
-      across = "#{line}\n| C | A | T |   |\n#{set_a}#{set_a}#{set_a}#{line}"
+      across = "#{line}\n| D | O | G |   |\n#{set_a}#{set_a}#{set_a}#{line}"
 
       # ----------------
-      # | C |   |   |   |
+      # | D |   |   |   |
       # ----------------
-      # | A |   |   |   |
+      # | O |   |   |   |
       # ----------------
-      # | T |   |   |   |
+      # | G |   |   |   |
       # ----------------
       # |   |   |   |   |
       # ----------------
-      down = "#{line}\n| C #{set_d}| A #{set_d}| T #{set_d}#{row.lstrip}#{line}"
+      down = "#{line}\n| D #{set_d}| O #{set_d}| G #{set_d}#{row.lstrip}#{line}"
 
+      # TODO: what's up with this??
       # must set equal to variable so it doesn't keep calling it to compare.
       board_as_string = test_board.print_board
       is_equal = board_as_string == down || board_as_string == across
       is_equal.must_equal true
     end
+
+    it 'Switches at random' do
+      line = "----------------"
+      row = "\n|   |   |   |   |\n"
+      set_a = "#{line}#{row}"
+      set_d = "|   |   |   |\n#{line}\n"
+
+      across = "#{line}\n| D | O | G |   |\n#{set_a}#{set_a}#{set_a}#{line}"
+      down = "#{line}\n| D #{set_d}| O #{set_d}| G #{set_d}#{row.lstrip}#{line}"
+
+      attempt_count = 0
+      across_count = 0
+      down_count = 0
+
+      while attempt_count < 10_000
+        attempt_count += 1
+        test_board = Scrabble::Board.new(4)
+        test_board.add_word(1, 1, "DOG")
+
+        board_as_string = test_board.print_board
+        if board_as_string == across
+          across_count += 1
+        elsif board_as_string == down
+          down_count += 1
+        end
+      end
+      max_diff_threshold = 500 # 10_000 * 0.05
+      actual_difference = (across_count - down_count).abs
+
+      (actual_difference < max_diff_threshold).must_equal true # assertion
+      (across_count + down_count).must_equal attempt_count # assertion
+    end
+
   end
 
   describe 'add word down' do
     it "adds a word to the board and returns 'true'" do
       word = "MAGGIE"
       test_board = Scrabble::Board.new()
-      test_board.add_word([5, 3], "MAGGIE").must_equal word
+      test_board.add_word(5, 3, "MAGGIE").must_equal word
     end
 
     it "returns 'word' if is forced along board edge" do
@@ -157,57 +191,57 @@ describe 'board' do
       # |   |   |   |   |   |   |   |   |   |   | M | A | G | G | I | E |
       # ----------------------------------------------------------------
       expected_board_end =  "| M | A | G | G | I | E |\n#{"----" * 16}"
-      test_board.add_word([16, 11], "MAGGIE").must_equal "MAGGIE"
+      test_board.add_word(16, 11, "MAGGIE").must_equal "MAGGIE"
 
       test_board.print_board.slice!(-90..-1).must_equal expected_board_end
+
+      test_board.add_word(1, 16, "HELLO").must_equal "HELLO"
     end
 
-    xit "returns 'word' if is forced into a valid position" do
+    it "returns 'word' if is forced into a valid position" do
       test_board = Scrabble::Board.new()
       word = "PUDDING"
-      test_board.add_word([5, 3], "MAGGIE")
-      test_board.add_word([5, 1], word).must_equal word
-      # puts test_board.print_board
+      test_board.add_word(5, 3, "MAGGIE")
+      test_board.add_word(5, 1, word).must_equal word
     end
 
     it "returns 'word' if is forced to overlap in valid position" do
       test_board = Scrabble::Board.new()
       word = "PUDDING"
-      test_board.add_word([16, 11], "MAGGIE").must_equal "MAGGIE"
-      test_board.add_word([10, 14], word).must_equal word
-      puts test_board.print_board
+      test_board.add_word(16, 11, "MAGGIE").must_equal "MAGGIE"
+      test_board.add_word(10, 14, word).must_equal word
     end
 
     it "returns 'nil' if provided coordinate outside range'" do
       test_board = Scrabble::Board.new()
-      assert_nil test_board.add_word([99, 3], "MAGGIE")
-      assert_nil test_board.add_word([3, 99], "MAGGIE")
-      assert_nil test_board.add_word([3, -1], "MAGGIE")
-      assert_nil test_board.add_word([-1, 12], "MAGGIE")
-      assert_nil test_board.add_word([0, 3], "MAGGIE")
-      assert_nil test_board.add_word([3, 0], "MAGGIE")
+      assert_raises{test_board.add_word(99, 3, "MAGGIE")}
+      assert_raises{ test_board.add_word(3, 99, "MAGGIE")}
+      assert_raises{ test_board.add_word(3, -1, "MAGGIE")}
+      assert_raises{ test_board.add_word(-1, 12, "MAGGIE")}
+      assert_raises{ test_board.add_word(0, 3, "MAGGIE")}
+      assert_raises{ test_board.add_word(3, 0, "MAGGIE")}
     end
 
     it "returns 'nil' if word is not a string'" do
       test_board = Scrabble::Board.new()
-      assert_nil test_board.add_word([5, 3], :maggie)
+      assert_raises{ test_board.add_word(5, 3, :maggie)}
     end
 
     it "returns 'nil' if word is empty'" do
       test_board = Scrabble::Board.new()
-      assert_nil test_board.add_word([5, 3], "")
+      assert_raises{ test_board.add_word(5, 3, "") }
     end
 
     it "returns 'nil' if word is contains any spaces'" do
       test_board = Scrabble::Board.new()
-      assert_nil test_board.add_word([5, 3], "   ")
-      assert_nil test_board.add_word([2, 3], "HELLO MAGGIE")
+      assert_raises{ test_board.add_word(5, 3, "   ")}
+      assert_raises{ test_board.add_word(2, 3, "HELLO MAGGIE") }
     end
 
     it "returns 'nil' if word is contains any non-letters'" do
       test_board = Scrabble::Board.new()
-      assert_nil test_board.add_word([2, 3], "HELLO,MAGGIE")
-      assert_nil test_board.add_word([2, 3], "M4GG1E")
+      assert_raises{ test_board.add_word(2, 3, "HELLO,MAGGIE") }
+      assert_raises{ test_board.add_word(2, 3, "M4GG1E") }
     end
 
 
